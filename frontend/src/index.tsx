36ae822e2b97ef210c5f7654578e9f56a659c1ba
Minje Sung <minje813@dgu.ac.kr>
2025-07-10 17:48:52 +0900

    feat: biome 설치 및 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom/client';
 import { css } from '@emotion/css';
+import ReactDOM from 'react-dom/client';
 
 const style = css`
   color: hotpink;
   font-size: 24px;
 `;
 
-const App = () => <div className={style}>Hello Emotion + React + TypeScript!</div>;
+const App = () => (
+  <div className={style}>Hello Emotion + React + TypeScript!</div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
