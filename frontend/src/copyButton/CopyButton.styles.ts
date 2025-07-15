febee886ce599dec8dc505de4d4a4dde1f5bb252
H0ngJu <hongju4385@gmail.com>
2025-07-15 15:30:08 +0900

    feat: CopyButton 구현

---
+++ frontend/src/copyButton/CopyButton.styles.ts
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { theme } from '../styles/theme';
+import type { CopyButtonVariant } from './variant';
+
+const color = {
+  filled: css`
+    background-color: ${theme.colors.gray06};
+    color: ${theme.colors.white};
+  `,
+  outlined: css`
+    background-color: ${theme.colors.white};
+    border: 1px solid ${theme.colors.gray06};
+    color: ${theme.colors.gray06};
+  `,
+};
+
+export const Wrapper = styled.button<{ $variant: CopyButtonVariant }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 50px;
+  width: 100%;
+  cursor: pointer;
+  ${({ $variant }) => color[$variant]}
+  ${({ theme }) => ({ ...theme.typography.buttonPrimary })}
+`;
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`;
---
+++ frontend/src/copyButton/CopyButton.tsx
@@ -0,0 +1,40 @@
+import { ReactComponent as CopyIcon } from '../@assets/icons/clipboard.svg';
+import * as S from './CopyButton.styles';
+import type { CopyButtonVariant } from './variant';
+
+interface CopyButtonProps {
+  /** 복사 버튼 라벨 */
+  label: string;
+  /** 복사할 문자열 */
+  copyText: string;
+  /** 복사 버튼 variant */
+  variant?: CopyButtonVariant;
+  /** 복사 아이콘 표시 여부 */
+  showIcon?: boolean;
+}
+
+const CopyButton = ({
+  label,
+  copyText,
+  variant = 'filled',
+  showIcon = true,
+}: CopyButtonProps) => {
+  return (
+    <S.Wrapper $variant={variant} onClick={() => copyLinkToClipboard(copyText)}>
+      <S.Container>
+        {label}
+        {showIcon && <CopyIcon />}
+      </S.Container>
+    </S.Wrapper>
+  );
+};
+
+const copyLinkToClipboard = async (copyText: string) => {
+  try {
+    await navigator.clipboard.writeText(copyText);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default CopyButton;
