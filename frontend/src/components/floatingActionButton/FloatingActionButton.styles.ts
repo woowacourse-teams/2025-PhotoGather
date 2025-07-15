3f97e34085091349fbcde9cebd4816a1242cdce0
H0ngJu <hongju4385@gmail.com>
2025-07-15 19:51:31 +0900

    design: active 속성 분리

--- frontend/src/components/floatingActionButton/FloatingActionButton.styles.ts
+++ frontend/src/components/floatingActionButton/FloatingActionButton.styles.ts
@@ -1,28 +1,28 @@
+import type { Theme } from '@emotion/react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { theme } from '../../styles/theme';
 import type { FloatingActionButtonType } from './variant';
 
-const color = {
+const color = (theme: Theme) => ({
   default: css`
     background-color: ${theme.colors.accent};
     color: ${theme.colors.gray06};
-
-    &:active {
-      background-color: ${theme.colors.darkAccent};
-      color: ${theme.colors.gray06};
-      box-shadow:
-        0px 2px 3px 0px rgba(0, 0, 0, 0.25) inset,
-        1px 1px 3px 0px #fff inset,
-        5px 5px 5px 0px rgba(0, 0, 0, 0.25);
-    }
   `,
   disabled: css`
     background-color: ${theme.colors.gray01};
     color: ${theme.colors.gray04};
     cursor: default;
   `,
-};
+});
+
+const active = (theme: Theme) => css`
+  background-color: ${theme.colors.darkAccent};
+  color: ${theme.colors.gray06};
+  box-shadow:
+    0px 2px 3px 0px rgba(0, 0, 0, 0.25) inset,
+    1px 1px 3px 0px #fff inset,
+    5px 5px 5px 0px rgba(0, 0, 0, 0.25);
+`;
 
 export const Wrapper = styled.button<{ $variant: FloatingActionButtonType }>`
   display: flex;
@@ -39,6 +39,13 @@ export const Wrapper = styled.button<{ $variant: FloatingActionButtonType }>`
     1px 1px 3px 0px #fff inset,
     5px 5px 5px 0px rgba(0, 0, 0, 0.25);
 
-  ${({ $variant }) => color[$variant]}
+  ${({ theme, $variant }) => color(theme)[$variant]}
   ${({ theme }) => ({ ...theme.typography.buttonPrimary })}
+  ${({ theme, $variant }) =>
+    $variant !== 'disabled' &&
+    css`
+    &:active {
+      ${active(theme)}
+    }
+  `}
 `;
