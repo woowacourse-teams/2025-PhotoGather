92a4c8630577372c8640ec1640cb9e718a54c160
ShinjungOh <tlswjd9260@gmail.com>
2025-07-16 00:17:01 +0900

    refactor: 스토리북 주석 추가

--- frontend/src/components/@common/CircularLabelButton/CircularLabelButton.tsx
+++ frontend/src/components/@common/CircularLabelButton/CircularLabelButton.tsx
@@ -1,7 +1,9 @@
 import * as S from './CircularLabelButton.styles';
 
 interface CircularLabelButton {
+  /** 아이콘 React Node */
   icon: React.ReactNode;
+  /** 아이콘 텍스트 */
   text: string;
 }
 
@@ -9,9 +11,7 @@ const CircularLabelButton = ({ icon, text }: CircularLabelButton) => {
   return (
     <S.Wrapper>
       <S.IconContainer>
-        <S.IconWrapper>
-          {icon}
-        </S.IconWrapper>
+        <S.IconWrapper>{icon}</S.IconWrapper>
       </S.IconContainer>
       <S.Text>{text}</S.Text>
     </S.Wrapper>
