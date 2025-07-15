39035d882219d5c142a3048a73ffe391cac26b70
ShinjungOh <tlswjd9260@gmail.com>
2025-07-15 15:44:59 +0900

    test: Button 스토리북 추가

--- frontend/src/components/@common/Button.tsx
+++ frontend/src/components/@common/Button.tsx
@@ -1,19 +1,24 @@
 import * as S from './Button.styles';
 
 interface ButtonProps {
+  /** 버튼의 variant */
   variant?: 'primary' | 'secondary' | 'tertiary';
+  /** 버튼 내부 텍스트 */
   text: string;
+  /** 버튼 클릭했을 때 실행할 함수*/
   onClick: () => void;
+  /** 버튼 활성화 여부*/
   disabled?: boolean;
 }
 
-const Button = ({ variant = 'primary', text, onClick, disabled = false }: ButtonProps) => {
+const Button = ({
+  variant = 'primary',
+  text,
+  onClick,
+  disabled = false,
+}: ButtonProps) => {
   return (
-    <S.StyledButton
-      variant={variant}
-      onClick={onClick}
-      disabled={disabled}
-    >
+    <S.StyledButton variant={variant} onClick={onClick} disabled={disabled}>
       {text}
     </S.StyledButton>
   );
