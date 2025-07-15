dd2eaf5b0949dc135418f62e8ff5959d1ed8e451
kjyyjk <rlawnsdud920@gmail.com>
2025-07-15 17:13:17 +0900

    feat: 사진 조회 구현

---
+++ backend/forgather/src/main/java/com/forgather/domain/space/repository/SpaceContentRepository.java
@@ -0,0 +1,8 @@
+package com.forgather.domain.space.repository;
+
+import com.forgather.domain.space.model.SpaceContent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpaceContentRepository extends JpaRepository<SpaceContent, Long> {
+}
