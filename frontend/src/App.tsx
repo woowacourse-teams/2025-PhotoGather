06ef85ed637a9a905845f86ae4407aefec35f0fb
hanheel <hanhee630630@gmail.com>
2025-07-15 11:19:28 +0900

    chore: emotion/styled 라이브러리로 교체

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
 import GlobalStyle from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 
@@ -6,10 +7,14 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>안녕하세요</div>
-      <div>Hello World</div>
+      <Test>안녕하세요</Test>
     </ThemeProvider>
   );
 };
+
+const Test = styled.div`
+  color: ${({ theme }) => theme.colors.primary};
+  ${({ theme }) => ({ ...theme.typography.buttonPrimary })}
+`;
 
 export default App;
