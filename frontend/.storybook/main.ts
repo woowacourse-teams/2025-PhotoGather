f5b5c2cf7fe6d3a5524a8d5d4b1900458ebcaf2a
hanheel <hanhee630630@gmail.com>
2025-07-16 16:17:56 +0900

    fix: storybook에서 node type을 인식하지 못하는 오류 해결

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,6 +1,6 @@
+import path from 'node:path';
 import type { TransformOptions } from '@babel/core';
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
