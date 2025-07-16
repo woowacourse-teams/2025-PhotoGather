2efa4b929b600b7c9da3d6ebef2d1d71355ed4de
Luna <97431021+ShinjungOh@users.noreply.github.com>
2025-07-16 12:50:29 +0900

    feature: Button 컴포넌트 제작 (#44)
    * feat: Button 컴포넌트 제작
    
    * test: Button 스토리북 추가
    
    * refactor: primary 버튼의 active 스타일 개선
    
    * refactor: Button의 type을 variant로 분리
    
    * refactor: 구조분해할당을 사용해 스타일 코드 간결화
    
    * refactor: variant별 스타일을 객체로 구조화

---
+++ frontend/src/components/@common/Button.tsx
@@ -0,0 +1,28 @@
+import * as S from './Button.styles';
+import type { ButtonVariant } from './variant';
+
+interface ButtonProps {
+  /** 버튼의 variant */
+  variant?: ButtonVariant;
+  /** 버튼 내부 텍스트 */
+  text: string;
+  /** 버튼 클릭했을 때 실행할 함수*/
+  onClick: () => void;
+  /** 버튼 활성화 여부*/
+  disabled?: boolean;
+}
+
+const Button = ({
+  variant = 'primary',
+  text,
+  onClick,
+  disabled = false,
+}: ButtonProps) => {
+  return (
+    <S.StyledButton $variant={variant} $disabled={disabled} onClick={onClick}>
+      {text}
+    </S.StyledButton>
+  );
+};
+
+export default Button;
