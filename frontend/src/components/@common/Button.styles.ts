a6a495051ea15dcd15314c942b7fd89fe0335609
ShinjungOh <tlswjd9260@gmail.com>
2025-07-16 12:02:38 +0900

    refactor: variant별 스타일을 객체로 구조화

--- frontend/src/components/@common/Button.styles.ts
+++ frontend/src/components/@common/Button.styles.ts
@@ -1,9 +1,77 @@
+import type { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { ButtonVariant } from './variant';
 
+const buttonStyles = {
+  primary: {
+    enabled: (theme: Theme) => `
+      border-radius: 12px;
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+      border: none;
+
+      &:active {
+        border-radius: 12px;
+        background: ${theme.colors.primary};
+        box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.60) inset;
+      }
+    `,
+    disabled: (theme: Theme) => `
+      border-radius: 12px;
+      background: ${theme.colors.gray01};
+      color: ${theme.colors.gray04};
+      border: none;
+    `,
+  },
+  secondary: {
+    enabled: (theme: Theme) => `
+      border-radius: 12px;
+      color: ${theme.colors.primary};
+      background: ${theme.colors.white};
+      border: 1px solid ${theme.colors.primary};
+
+      &:active {
+        border-radius: 12px;
+        background: ${theme.colors.primary10}; 
+        border: 1px solid ${theme.colors.primary};
+      }
+    `,
+    disabled: (theme: Theme) => `
+      border-radius: 12px;
+      color: ${theme.colors.gray04};
+      background: ${theme.colors.gray01};
+      border: 1px solid ${theme.colors.gray03};
+    `,
+  },
+  tertiary: {
+    enabled: (theme: Theme) => `
+      background: transparent;
+      border: none;
+      color: ${theme.colors.gray03};
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 120%;
+
+      &:active {
+        color: ${theme.colors.primary};
+      }
+    `,
+    disabled: (theme: Theme) => `
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 120%;
+      background: transparent;
+      color: ${theme.colors.gray01};
+      border: none;
+    `,
+  },
+};
+
 export const StyledButton = styled.button<{
-  variant: ButtonVariant;
-  disabled: boolean;
+  $variant: ButtonVariant;
+  $disabled: boolean;
 }>`
   display: flex;
   padding: 12px 20px;
@@ -13,85 +81,10 @@ export const StyledButton = styled.button<{
 
   ${({ theme }) => ({ ...theme.typography.buttonPrimary })}
 
-  ${({ variant, theme, disabled }) =>
-    variant === 'primary' &&
-    !disabled &&
-    `
-    border-radius: 12px;
-    background: ${theme.colors.primary};
-    color: ${theme.colors.white};
-    border: none;
+  ${({ $variant, theme, $disabled }) => {
+    const variantStyles = buttonStyles[$variant];
+    const state = $disabled ? 'disabled' : 'enabled';
 
-    &:active {
-      border-radius: 12px;
-      background: ${theme.colors.primary};
-      box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.60) inset;
-    }
-  `}
-
-  ${({ variant, theme, disabled }) =>
-    variant === 'primary' &&
-    disabled &&
-    `
-    border-radius: 12px;
-    background: ${theme.colors.gray01};
-    color: ${theme.colors.gray04};
-    border: none;
-  `}
-
-  ${({ variant, theme, disabled }) =>
-    variant === 'secondary' &&
-    !disabled &&
-    `
-    border-radius: 12px;
-    color: ${theme.colors.primary};
-    background: ${theme.colors.white};
-    border: 1px solid ${theme.colors.primary};
-
-    &:active {
-      border-radius: 12px;
-      background: ${theme.colors.primary10}; 
-      border: 1px solid ${theme.colors.primary};
-    }
-  `}
-
-  ${({ variant, theme, disabled }) =>
-    variant === 'secondary' &&
-    disabled &&
-    `
-    border-radius: 12px;
-    color: ${theme.colors.gray04};
-    background: ${theme.colors.gray01};
-    border: 1px solid ${theme.colors.gray03};
-  `}
-
-  ${({ variant, theme, disabled }) =>
-    variant === 'tertiary' &&
-    !disabled &&
-    `
-    background: transparent;
-    border: none;
-    color: ${theme.colors.gray03};
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 120%;
-
-    &:active {
-      color: ${theme.colors.primary};
-    }
-  `}
-
-  ${({ variant, theme, disabled }) =>
-    variant === 'tertiary' &&
-    disabled &&
-    `
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 120%;
-    background: transparent;
-    color: ${theme.colors.gray01};
-    border: none;
-  `}
+    return variantStyles[state](theme);
+  }}
 `;
--- frontend/src/components/@common/Button.tsx
+++ frontend/src/components/@common/Button.tsx
@@ -19,7 +19,7 @@ const Button = ({
   disabled = false,
 }: ButtonProps) => {
   return (
-    <S.StyledButton variant={variant} onClick={onClick} disabled={disabled}>
+    <S.StyledButton $variant={variant} $disabled={disabled} onClick={onClick}>
       {text}
     </S.StyledButton>
   );
