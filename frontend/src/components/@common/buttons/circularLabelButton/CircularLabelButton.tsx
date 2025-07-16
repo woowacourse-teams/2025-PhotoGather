4951d2cf85607c5b50c63d354dcb006945977738
Minje Sung <minje813@dgu.ac.kr>
2025-07-16 22:12:10 +0900

    refactor: Button, CircularLabelButton, CopyButton 버튼 기본 속성 추가

--- frontend/src/components/@common/buttons/circularLabelButton/CircularLabelButton.tsx
+++ frontend/src/components/@common/buttons/circularLabelButton/CircularLabelButton.tsx
@@ -1,16 +1,21 @@
 import * as S from './CircularLabelButton.styles';
 
-interface CircularLabelButtonProps {
+interface CircularLabelButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /** 아이콘 React Node */
   icon: React.ReactNode;
   /** 아이콘 텍스트 */
   text: string;
 }
 
-const CircularLabelButton = ({ icon, text }: CircularLabelButtonProps) => {
+const CircularLabelButton = ({
+  icon,
+  text,
+  ...buttonProps
+}: CircularLabelButtonProps) => {
   return (
     <S.Wrapper>
-      <S.IconContainer>{icon}</S.IconContainer>
+      <S.IconContainer {...buttonProps}>{icon}</S.IconContainer>
       <S.Text>{text}</S.Text>
     </S.Wrapper>
   );
