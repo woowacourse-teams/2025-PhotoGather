152f07941110ed2e11b459797a1be399fd0cbb3e
hanheel <hanhee630630@gmail.com>
2025-07-16 23:04:36 +0900

    refactor: highlightText props interface 수정
    textAlign, textColorStyle 등 optional props 추가

--- frontend/src/components/@common/highlightText/HighlightText.styles.ts
+++ frontend/src/components/@common/highlightText/HighlightText.styles.ts
@@ -1,16 +1,24 @@
 import type { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div<{ $fontStyle: keyof Theme['typography'] }>`
+export type TextAlign = 'left' | 'center' | 'right';
+
+export const Wrapper = styled.div<{
+  $fontStyle: keyof Theme['typography'];
+  $textAlign: TextAlign;
+}>`
   ${({ theme, $fontStyle }) => ({ ...theme.typography[$fontStyle] })}
   white-space: pre-line;
-  text-align: center;
+  text-align: ${({ $textAlign }) => $textAlign ?? 'center'};
 `;
 
-export const Text = styled.span`
-  color: ${({ theme }) => theme.colors.gray06};
+export const Text = styled.span<{ $textColorStyle: keyof Theme['colors'] }>`
+  color: ${({ theme, $textColorStyle }) => theme.colors[$textColorStyle]};
 `;
 
-export const HighLight = styled.span`
-  color: ${({ theme }) => theme.colors.primary};
+export const HighLight = styled.span<{
+  $highlightColorStyle: keyof Theme['colors'];
+}>`
+  color: ${({ theme, $highlightColorStyle }) =>
+    theme.colors[$highlightColorStyle]};
 `;
--- frontend/src/components/@common/highlightText/HighlightText.tsx
+++ frontend/src/components/@common/highlightText/HighlightText.tsx
@@ -4,30 +4,51 @@ import * as S from './HighlightText.styles';
 interface HighlightTextProps {
   /** 전체 문장 */
   text: string;
-  /** 강조할 부분 문자열 */
-  highlightText: string;
-  /** 폰트 스타일 키 */
+  /** 강조할 부분 문자열 배열*/
+  highlightTextArray: string[];
+  /** 폰트 스타일 */
   fontStyle: keyof Theme['typography'];
+  /** 강조할 부분 색상 */
+  highlightColorStyle: keyof Theme['colors'];
+  /** 일반 텍스트 색상 */
+  textColorStyle?: keyof Theme['colors'];
+  /** 텍스트 정렬 */
+  textAlign?: S.TextAlign;
 }
 
 const HighlightText = ({
   text,
-  highlightText,
+  highlightTextArray,
   fontStyle,
+  textColorStyle,
+  highlightColorStyle,
+  textAlign,
 }: HighlightTextProps) => {
-  const normalTextArray = text.split(highlightText);
+  const splitStandard = new RegExp(`(${highlightTextArray.join('|')})`, 'g');
+  const tokens = text.split(splitStandard);
 
   return (
-    <>
-      {normalTextArray.map((text, index) => (
-        <S.Wrapper key={text} $fontStyle={fontStyle}>
-          <S.Text>{text}</S.Text>
-          {index !== normalTextArray.length - 1 && (
-            <S.HighLight>{highlightText}</S.HighLight>
-          )}
-        </S.Wrapper>
-      ))}
-    </>
+    <S.Wrapper
+      key={text}
+      $fontStyle={fontStyle}
+      $textAlign={textAlign ?? 'center'}
+    >
+      {tokens.map((token, index) =>
+        highlightTextArray.includes(token) ? (
+          <S.HighLight
+            // biome-ignore lint/suspicious/noArrayIndexKey: index가 고유 키가 되어야 함
+            key={index}
+            $highlightColorStyle={highlightColorStyle}
+          >
+            {token}
+          </S.HighLight>
+        ) : (
+          <S.Text key={token} $textColorStyle={textColorStyle ?? 'gray06'}>
+            {token}
+          </S.Text>
+        ),
+      )}
+    </S.Wrapper>
   );
 };
 
