b0c4ea83765df55d2c0abc7c43b7d8e8a6553b0e
hanheel <hanhee630630@gmail.com>
2025-07-16 23:36:01 +0900

    refactor: imageGrid /imageElement의 스타일 수정 및 size props 삭제
    1. background color 정의
    2. border radius 정의
    3. 200으로 하드코딩 돼있던 size props 삭제

--- frontend/src/components/@common/imageGrid/ImageGrid.styles.ts
+++ frontend/src/components/@common/imageGrid/ImageGrid.styles.ts
@@ -6,4 +6,6 @@ export const Wrapper = styled.div<{ $rowImageAmount: number }>`
   grid-template-columns: repeat(${({ $rowImageAmount }) => $rowImageAmount}, 1fr);
   gap: 4px;
   width: fit-content;
+  background-color: ${({ theme }) => theme.colors.grayBackground};
+  border-radius: 12px 12px 0 0;
 `;
