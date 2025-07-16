e32e4a7dfa7e97ba508a2069fe04ea01ff75bc42
Hacanna42 <dtmdgus901@gmail.com>
2025-07-16 17:19:56 +0900

    feat: S3 업로드 구현 및 연동

---
+++ backend/forgather/src/main/java/com/forgather/domain/space/service/AwsS3Service.java
@@ -0,0 +1,38 @@
+package com.forgather.domain.space.service;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.forgather.global.config.S3Properties;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+@RequiredArgsConstructor
+public class AwsS3Service {
+
+    private final S3Client s3Client;
+    private final S3Properties s3Properties;
+
+    public void upload(String hostCode, MultipartFile file) throws IOException {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+            .bucket(s3Properties.getBucketName())
+            .key(generateFilePath(hostCode, file))
+            .tagging(s3Properties.getTagging())
+            .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+    }
+
+    private String generateFilePath(String hostCode, MultipartFile file) {
+        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        String uuid = UUID.randomUUID().toString();
+        return String.format("/%s/%s/%s.%s", s3Properties.getRootDirectory(), hostCode, uuid, extension);
+    }
+}
