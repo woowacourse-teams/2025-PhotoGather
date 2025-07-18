ea13e02afa28545feeb69dc5fee56b62cb561332
H0ngJu <hongju4385@gmail.com>
2025-07-18 21:20:43 +0900

    refactor: uploadBox가 hasImages에 따라 flex되도록 수정

--- frontend/src/pages/manager/imageUploadPage/ImageUploadPage.styles.ts
+++ frontend/src/pages/manager/imageUploadPage/ImageUploadPage.styles.ts
@@ -4,11 +4,12 @@ export const Wrapper = styled.div<{ $hasImages: boolean }>`
   ${({ theme }) => theme.typography.header03}
   width: 100%;
   height: 100%;
+  //TODO: height 고정 값 때문에 의도적으로 top,bottom padding을 빼준 값 사용
+  min-height: calc(100dvh - 64px);
   border-radius: 12px;
   display: flex;
   flex-direction: column;
   gap: 16px;
-  //TODO: page scroll 해결 후 UploadBox padding 조정 필요
 `;
 
 export const TitleContainer = styled.div`
@@ -30,6 +31,7 @@ export const Description = styled.p`
 
 export const UploadContainer = styled.div<{ $hasImages: boolean }>`
   flex: ${({ $hasImages }) => ($hasImages ? 0 : 1)};
+  height: 100%;
   display: flex;
   margin-bottom: 16px;
 `;
