e25cde61cf4f05fbd3938f3d23ef9427cede0b90
H0ngJu <hongju4385@gmail.com>
2025-07-18 11:17:42 +0900

    refactor: storybook 캡션 수정

--- frontend/src/components/uploadBox/UploadBox.tsx
+++ frontend/src/components/uploadBox/UploadBox.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as CameraIcon } from '@assets/icons/camera.svg';
 import * as S from './UploadBox.styles';
 
 interface UploadBoxProps {
-  /**박스 내 들어갈 텍스트 */
+  /** 박스 내 들어갈 텍스트 */
   text: string;
 }
 
