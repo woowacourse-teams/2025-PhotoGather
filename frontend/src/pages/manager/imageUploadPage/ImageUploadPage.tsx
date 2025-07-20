36dd9773ba947228ea01cb2acd680098ea441d52
H0ngJu <hongju4385@gmail.com>
2025-07-21 00:12:10 +0900

    feat: file, image를 받아서 grid에 전달 및 핸들러 추가

--- frontend/src/pages/manager/imageUploadPage/ImageUploadPage.tsx
+++ frontend/src/pages/manager/imageUploadPage/ImageUploadPage.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import FloatingActionButton from '../../../components/@common/buttons/floatingActionButton/FloatingActionButton';
 import HighlightText from '../../../components/@common/highlightText/HighlightText';
 import ImageGrid from '../../../components/@common/imageGrid/ImageGrid';
 import SpaceHeader from '../../../components/spaceHeader/SpaceHeader';
 import UploadBox from '../../../components/uploadBox/UploadBox';
 import * as S from './ImageUploadPage.styles';
-import { mockImageList, mockSpaceData } from './mockSpaceData';
+import { mockSpaceData } from './mockSpaceData';
 
 const ImageUploadPage = () => {
-  const hasImages = Array.isArray(mockImageList) && mockImageList.length > 0;
+  const [imageFiles, setImageFiles] = useState<File[]>([]);
+  const [previewUrls, setPreviewUrls] = useState<string[]>([]);
+  const hasImages = Array.isArray(previewUrls) && previewUrls.length > 0;
   const uploadBoxText = '함께한 순간을 올려주세요';
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files || []);
+    setImageFiles([...imageFiles, ...files]);
+
+    const urls = files.map((file) => URL.createObjectURL(file));
+    setPreviewUrls([...previewUrls, ...urls]);
+  };
 
   return (
     <S.Wrapper $hasImages={hasImages}>
@@ -18,20 +29,24 @@ const ImageUploadPage = () => {
       />
 
       <S.UploadContainer $hasImages={hasImages}>
-        <UploadBox text={uploadBoxText} iconSize={hasImages ? 60 : 100} />
+        <UploadBox
+          text={uploadBoxText}
+          iconSize={hasImages ? 60 : 100}
+          onChange={handleImageUpload}
+        />
       </S.UploadContainer>
 
       {hasImages && (
         <>
-          <ImageGrid imageUrlList={mockImageList} rowImageAmount={3} />
+          <ImageGrid imageUrlList={previewUrls} rowImageAmount={3} />
           <S.ButtonContainer>
             <FloatingActionButton
               label={
                 <HighlightText
-                  text={`사진 ${mockImageList.length}장 업로드하기`}
+                  text={`사진 ${previewUrls.length}장 업로드하기`}
                   fontStyle="buttonPrimary"
                   highlightColorStyle="gray04"
-                  highlightTextArray={[`사진 ${mockImageList.length}장`]}
+                  highlightTextArray={[`사진 ${previewUrls.length}장`]}
                 />
               }
             />
