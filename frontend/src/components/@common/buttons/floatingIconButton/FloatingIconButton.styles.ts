be3d1e0e8d3fd1778dc1ebae42497babd54dc663
Luna <97431021+ShinjungOh@users.noreply.github.com>
2025-07-20 18:35:50 +0900

    feature: FloatingIconButton 제작 (#100)
    * feat: FloatingIconButton 컴포넌트 제작
    
    * test: FloatingIconButton 스토리북 작성
    
    * refactor: 불필요 스타일 속성 제거
    
    * refactor: FloatingIconButton의 props로 icon을 넘겨주도록 로직 변경

---
+++ frontend/src/components/@common/buttons/floatingIconButton/FloatingIconButton.styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.colors.gray03};
+  background-color: ${({ theme }) => theme.colors.white};
+  filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.25));
+  overflow: hidden;
+`;
