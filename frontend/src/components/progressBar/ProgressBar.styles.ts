d9eb179c54f8035f55f854b767e1876e842fef2c
Minje Sung <minje813@dgu.ac.kr>
2025-07-21 00:40:44 +0900

    feat: ProgressBar 기능 구현

--- frontend/src/components/progressBar/ProgressBar.styles.ts
+++ frontend/src/components/progressBar/ProgressBar.styles.ts
@@ -1,5 +1,53 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+
+// 왼쪽에서 오른쪽으로 scale 증가
+const fillAnimation = keyframes`
+  from {
+    transform: scaleX(0);
+  }
+  to {
+    transform: scaleX(1);
+  }
+`;
+
+// 오른쪽에서 왼쪽으로 scale 감소
+const emptyAnimation = keyframes`
+  from {
+    transform: scaleX(1);
+  }
+  to {
+    transform: scaleX(0);
+  }
+`;
 
 export const Wrapper = styled.div`
   width: 100%;
+  display: flex;
+  gap: 8px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProgressElement = styled.div<{ $isFilled: boolean }>`
+  position: relative;
+  flex: 1;
+  height: 5px;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.gray01};
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.primary60};
+    border-radius: 20px;
+    transform: scaleX(${({ $isFilled }) => ($isFilled ? 1 : 0)});
+    transform-origin: left;
+    animation: ${({ $isFilled }) => ($isFilled ? fillAnimation : emptyAnimation)} 0.5s forwards;
+  }
 `;
--- frontend/src/components/progressBar/ProgressBar.tsx
+++ frontend/src/components/progressBar/ProgressBar.tsx
@@ -1,14 +1,22 @@
 import * as S from './ProgressBar.styles';
 
 interface ProgressBarProps {
+  /** 현재 단계값 */
   step: number;
+  /** 모든 단계값 */
   maxStep: number;
 }
 
 const ProgressBar = ({ step, maxStep }: ProgressBarProps) => {
   return (
     <S.Wrapper>
-      <p>프로그래스바임</p>
+      {Array.from({ length: maxStep }).map((_, index) => {
+        const isFilled = index + 1 <= step;
+        return (
+          // biome-ignore lint/suspicious/noArrayIndexKey: false positive
+          <S.ProgressElement key={index} $isFilled={isFilled} />
+        );
+      })}
     </S.Wrapper>
   );
 };
