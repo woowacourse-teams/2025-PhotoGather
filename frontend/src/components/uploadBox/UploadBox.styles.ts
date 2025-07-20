3414120c18d6da1eff76c48514d0e0e659580e7d
H0ngJu <hongju4385@gmail.com>
2025-07-20 23:59:36 +0900

    feat: UploadBox가 file을 선택할 수 있도록 수정

--- frontend/src/components/uploadBox/UploadBox.styles.ts
+++ frontend/src/components/uploadBox/UploadBox.styles.ts
@@ -1,8 +1,13 @@
 import { ReactComponent as CameraIcon } from '@assets/icons/camera.svg';
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.label`
   width: 100%;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
   color: ${({ theme }) => theme.colors.primary60};
   background-color: ${({ theme }) => theme.colors.white};
   ${({ theme }) => ({ ...theme.typography.header03 })}
@@ -14,6 +19,7 @@ export const Wrapper = styled.div`
   align-items: center;
   gap: 20px;
   padding: 25px;
+  cursor: pointer;
 `;
 
 export const Icon = styled(CameraIcon)<{ iconSize: number }>`
--- frontend/src/components/uploadBox/UploadBox.tsx
+++ frontend/src/components/uploadBox/UploadBox.tsx
@@ -5,13 +5,18 @@ interface UploadBoxProps {
   text: string;
   /** 박스 내 아이콘 크기 */
   iconSize?: number;
+  /** 파일 선택 시이벤트 핸들러 */
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const UploadBox = ({ text, iconSize = 100 }: UploadBoxProps) => {
+const UploadBox = ({ text, iconSize = 100, onChange }: UploadBoxProps) => {
   return (
-    <S.Wrapper>
-      <S.Icon iconSize={iconSize} />
-      {text}
+    <S.Wrapper htmlFor="file-input">
+      <S.Container>
+        <S.Icon iconSize={iconSize} />
+        {text}
+      </S.Container>
+      <input id="file-input" type="file" hidden onChange={onChange} />
     </S.Wrapper>
   );
 };
