06b5215c1e7f0cb224893e528393b3c33773fc88
ShinjungOh <tlswjd9260@gmail.com>
2025-07-20 18:20:52 +0900

    refactor: FloatingIconButton의 props로 icon을 넘겨주도록 로직 변경

--- frontend/src/components/@common/buttons/floatingIconButton/FloatingIconButton.tsx
+++ frontend/src/components/@common/buttons/floatingIconButton/FloatingIconButton.tsx
@@ -1,20 +1,21 @@
-import { ReactComponent as UpwardArrow } from '@assets/icons/upwardArrow.svg';
-import { theme } from '../../../../styles/theme';
 import * as S from './FloatingIconButton.styles';
 
 interface FloatingIconButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** 아이콘 React Node */
+  icon: React.ReactNode;
   /** 버튼 클릭했을 때 실행할 함수*/
   onClick: () => void;
 }
 
 const FloatingIconButton = ({
+  icon,
   onClick,
   ...buttonProps
 }: FloatingIconButtonProps) => {
   return (
     <S.Wrapper onClick={onClick} {...buttonProps}>
-      <UpwardArrow fill={theme.colors.gray03} />
+      {icon}
     </S.Wrapper>
   );
 };
