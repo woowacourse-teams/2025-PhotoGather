70a616a84469e707cf79c0e090c2454d1eb7fbc6
hanheel <hanhee630630@gmail.com>
2025-07-21 14:07:50 +0900

    refactor: 제목과 날짜 사이의 gap 수정

--- frontend/src/components/spaceHeader/SpaceHeader.styles.ts
+++ frontend/src/components/spaceHeader/SpaceHeader.styles.ts
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
 
 export const TitleIconContainer = styled.div`
   display: flex;
