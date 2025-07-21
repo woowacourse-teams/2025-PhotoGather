a8d1cad428bcfdbb77a31bcc667c943d82c3dfbf
H0ngJu <hongju4385@gmail.com>
2025-07-21 16:12:37 +0900

    refactor: 업로드 박스 default, active 색상코드 분리

--- frontend/src/components/uploadBox/UploadBox.styles.ts
+++ frontend/src/components/uploadBox/UploadBox.styles.ts
@@ -10,7 +10,8 @@ export const Container = styled.div<{ $isActive: boolean }>`
   width: 100%;
   height: 100%;
   color: ${({ theme }) => theme.colors.primary20};
-  background-color: ${({ $isActive }) => ($isActive ? '#F0EBFCB3' : '#F0EBFC66')};
+  background-color: ${({ theme, $isActive }) =>
+    $isActive ? theme.colors.uploadBox.active : theme.colors.uploadBox.default};
   ${({ theme }) => ({ ...theme.typography.header03 })}
   border-radius: 12px;
   display: flex;
