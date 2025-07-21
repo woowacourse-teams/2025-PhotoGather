fd3d09ed4a120654e99af37bb0a5bea9de3abfad
송선권 <songsunkook@gmail.com>
2025-07-21 16:59:49 +0900

    fix: swagger server url 프로토콜 수정 (#111)

--- backend/forgather/src/main/java/com/forgather/global/config/SwaggerConfig.java
+++ backend/forgather/src/main/java/com/forgather/global/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package com.forgather.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,14 +16,17 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+            .info(apiInfo())
+            .components(new Components())
+            .servers(List.of(
+                new Server().url("https://api.forgather.com").description("Production Server")
+            ));
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Forgather API")
-                .description("당신을 위한 순간, 흩어지지 않게. Forgather")
-                .version("1.0.0");
+            .title("Forgather API")
+            .description("당신을 위한 순간, 흩어지지 않게. Forgather")
+            .version("1.0.0");
     }
 }
