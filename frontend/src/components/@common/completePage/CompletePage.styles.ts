41c8fec3871f38fcb546c0065e9584ac8ca06abd
ShinjungOh <tlswjd9260@gmail.com>
2025-07-22 19:11:42 +0900

    feat: 완료 페이지 UI 구현

--- frontend/src/components/@common/completePage/CompletePage.styles.ts
+++ frontend/src/components/@common/completePage/CompletePage.styles.ts
@@ -1,5 +1,49 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-    background-color: #cdf6bd;
+  width: 100%;
+  height: 100%;
+  min-height: ${({ theme }) => `calc(100dvh - ${parseInt(theme.layout.padding.topBottom) * 2}px)`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+`;
+
+export const Icon = styled.img`
+  width: 120px;
+  height: 120px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const Description = styled.p`
+  ${({ theme }) => ({ ...theme.typography.bodyRegular })}
+  color: ${({ theme }) => theme.colors.gray03};
+`;
+
+export const BottomContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
 `;
--- frontend/src/components/@common/completePage/CompletePage.tsx
+++ frontend/src/components/@common/completePage/CompletePage.tsx
@@ -1,43 +1,35 @@
+import { COMPLETE_PAGE_VARIANTS } from '../../../constants/messages';
+import type { CompletePageVariant } from '../../../types/completePage.types';
 import Button from '../buttons/button/Button';
+import HighlightText from '../highlightText/HighlightText';
 import * as S from './CompletePage.styles';
-
-export type CompletePageType = 'upload' | 'download' | 'spaceCreated';
-
-export const COMPLETE_PAGE_VARIANTS = {
-  upload: {
-    image: '@assets/images/upload.png',
-    title: '추억을 전달했어요',
-    description: '업로드가 완료됐어요',
-    buttonText: '이어서 업로드하기',
-  },
-  download: {
-    image: '@assets/images/download.png',
-    title: '추억을 저장했어요',
-    description: '다운로드를 완료했어요',
-    buttonText: '나의 스페이스로 이동',
-  },
-  spaceCreated: {
-    image: '@assets/images/space_create.png',
-    title: '스페이스가 완성됐어요',
-    description: '내 스페이스로 이동해 볼까요?',
-    buttonText: '나의 스페이스로 이동',
-  },
-} as const;
 
 interface CompletePageProps {
   /** 완료 페이지 타입 */
-  type: CompletePageType;
+  variant: CompletePageVariant;
 }
 
-const CompletePage = ({ type }: CompletePageProps) => {
-  const mappedType = COMPLETE_PAGE_VARIANTS[type];
+const CompletePage = ({ variant }: CompletePageProps) => {
+  const mappedType = COMPLETE_PAGE_VARIANTS[variant];
 
   return (
     <S.Wrapper>
-      <img src={mappedType.image} alt={mappedType.title} />
-      <h1>{mappedType.title}</h1>
-      <p>{mappedType.description}</p>
-      <Button text={mappedType.buttonText} onClick={() => console.log(1)} />
+      <S.ContentWrapper>
+        <S.Icon src={mappedType.IMAGE} alt={mappedType.TITLE} />
+        <S.TextContainer>
+          <HighlightText
+            text={mappedType.TITLE}
+            fontStyle="header02"
+            highlightTextArray={[mappedType.HIGHLIGHT_WORDS]}
+            highlightColorStyle="primary"
+          />
+          <S.Description>{mappedType.DESCRIPTION}</S.Description>
+        </S.TextContainer>
+      </S.ContentWrapper>
+
+      <S.BottomContainer>
+        <Button text={mappedType.BUTTON_TEXT} onClick={() => console.log(1)} />
+      </S.BottomContainer>
     </S.Wrapper>
   );
 };
