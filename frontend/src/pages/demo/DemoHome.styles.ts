e9e6cbd140e895086edefc947fe3acb24e8352c2
Minje Sung <minje813@dgu.ac.kr>
2025-07-23 20:36:08 +0900

    style: 스타일 파일 prettier 적용

--- frontend/src/pages/demo/DemoHome.styles.ts
+++ frontend/src/pages/demo/DemoHome.styles.ts
@@ -1,17 +1,17 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-    width: 100%;
-    height: ${({ theme }) => `calc(100dvh - ${parseInt(theme.layout.padding.topBottom) * 2}px)`};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 24px;
+  width: 100%;
+  height: ${({ theme }) => `calc(100dvh - ${parseInt(theme.layout.padding.topBottom) * 2}px)`};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
 `;
 
 export const Title = styled.p`
-    ${({ theme }) => ({ ...theme.typography.header01 })};
+  ${({ theme }) => ({ ...theme.typography.header01 })};
 `;
 
 export const Icon = styled.img`
