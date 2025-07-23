94af77c754a52539c58bd1a2e6cff2e85b22beef
Minje Sung <minje813@dgu.ac.kr>
2025-07-23 14:15:11 +0900

    refactor: step -> currentStep으로 인자명 수정

--- frontend/src/components/progressBar/ProgressBar.tsx
+++ frontend/src/components/progressBar/ProgressBar.tsx
@@ -2,16 +2,16 @@ import * as S from './ProgressBar.styles';
 
 interface ProgressBarProps {
   /** 현재 단계값 */
-  step: number;
+  currentStep: number;
   /** 모든 단계값 */
   maxStep: number;
 }
 
-const ProgressBar = ({ step, maxStep }: ProgressBarProps) => {
+const ProgressBar = ({ currentStep, maxStep }: ProgressBarProps) => {
   return (
     <S.Wrapper>
       {Array.from({ length: maxStep }).map((_, index) => {
-        const isFilled = index + 1 <= step;
+        const isFilled = index + 1 <= currentStep;
         return (
           <S.ProgressElement
             // biome-ignore lint/suspicious/noArrayIndexKey: false positive
--- frontend/src/stories/ProgressBar.stories.tsx
+++ frontend/src/stories/ProgressBar.stories.tsx
@@ -11,7 +11,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    step: 1,
+    currentStep: 1,
     maxStep: 4,
   },
 };
