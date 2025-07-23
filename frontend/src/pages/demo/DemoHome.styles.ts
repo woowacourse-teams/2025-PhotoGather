63aa173c3e7b0769f20d0e33705b0aad14c040eb
Minje Sung <minje813@dgu.ac.kr>
2025-07-23 20:23:23 +0900

    feat: 데모 페이지 요소 구성

---
+++ frontend/src/pages/demo/DemoHome.styles.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: ${({ theme }) => `calc(100dvh - ${parseInt(theme.layout.padding.topBottom) * 2}px)`};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+`;
+
+export const Title = styled.p`
+    ${({ theme }) => ({ ...theme.typography.header01 })};
+`;
+
+export const Icon = styled.img`
+  width: 120px;
+  height: 120px;
+`;
--- frontend/src/pages/demo/DemoHome.tsx
+++ frontend/src/pages/demo/DemoHome.tsx
@@ -1,8 +1,25 @@
+import rocketImage from '@assets/images/rocket.png';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/@common/buttons/button/Button';
+import { ROUTES } from '../../constants/routes';
+import * as S from './DemoHome.styles';
+
 const DemoHome = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>데모 메인페이지임</p>
-    </div>
+    <S.Wrapper>
+      <S.Icon src={rocketImage} alt="데모 페이지 아이콘"></S.Icon>
+      <S.Title>Forgather 데모</S.Title>
+      <Button
+        text="(GUEST) 스페이스 업로드 페이지"
+        onClick={() => navigate(ROUTES.GUEST.IMAGE_UPLOAD)}
+      />
+      <Button
+        text="(MANAGER) 스페이스 관리 페이지 이동"
+        onClick={() => navigate(ROUTES.MANAGER.SPACE_HOME)}
+      />
+    </S.Wrapper>
   );
 };
 
