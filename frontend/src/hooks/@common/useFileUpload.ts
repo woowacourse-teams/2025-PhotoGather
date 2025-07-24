720c016ca4800d2a1b7fbeff33bfa4dc52a5dfe6
H0ngJu <hongju4385@gmail.com>
2025-07-24 20:38:39 +0900

    fix: 파일 선택 시 이미지만 업로드 가능하도록 수정

--- frontend/src/hooks/@common/useFileUpload.ts
+++ frontend/src/hooks/@common/useFileUpload.ts
@@ -9,11 +9,20 @@ const useFileUpload = () => {
     setPreviewUrls((prev) => [...prev, ...urls]);
   };
 
+  const isImageFile = (file: File) => file.type.startsWith('image/');
+
   const handleFilesUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     //TODO: 파일 업로드 최대 용량 제한 | 업로드 최대 개수 제한?
     const files = Array.from(event.target.files || []);
-    setImageFiles((prev) => [...prev, ...files]);
-    addPreviewUrlsFromFiles(files);
+    const validFiles = files.filter(isImageFile);
+    const invalidFiles = files.filter((file) => !isImageFile(file));
+    if (invalidFiles.length > 0) {
+      alert(
+        `이미지 파일만 업로드 가능해요. 파일을 다시 확인해주세요.\n${invalidFiles.map((file) => file.name).join('\n')}`,
+      );
+    }
+    setImageFiles((prev) => [...prev, ...validFiles]);
+    addPreviewUrlsFromFiles(validFiles);
   };
 
   const handleFilesDrop = (event: React.DragEvent<HTMLLabelElement>) => {
