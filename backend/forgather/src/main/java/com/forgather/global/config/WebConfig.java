df5b8be5be18eba8cf22a2df2846bba4cd063169
kjyyjk <rlawnsdud920@gmail.com>
2025-07-24 17:59:21 +0900

    fix: 페이징 번호 1번을 기본으로 설정 (#177)

--- backend/forgather/src/main/java/com/forgather/global/config/WebConfig.java
+++ backend/forgather/src/main/java/com/forgather/global/config/WebConfig.java
@@ -1,6 +1,10 @@
 package com.forgather.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedHeaders(corsProperties.getAllowedHeaders().toArray(new String[0]))
             .allowCredentials(corsProperties.isAllowCredentials())
             .maxAge(corsProperties.getMaxAge());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setOneIndexedParameters(true); // 1부터 시작
+        resolvers.add(pageableResolver);
     }
 }
