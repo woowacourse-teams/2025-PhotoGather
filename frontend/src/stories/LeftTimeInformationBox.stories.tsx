92cfc6990e795f0373ef3f40b2f4f68738da1c50
ShinjungOh <tlswjd9260@gmail.com>
2025-07-30 15:02:24 +0900

    test: LeftTimeInformationBox 스토리북 내용 추가

--- frontend/src/stories/LeftTimeInformationBox.stories.tsx
+++ frontend/src/stories/LeftTimeInformationBox.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import LeftTimeInformationBox from '../components/leftTimeInformationBox/LeftTimeInformationBox';
 
 const meta: Meta<typeof LeftTimeInformationBox> = {
-  title: 'components/InformationConfirmBox',
+  title: 'components/LeftTimeInformationBox',
   component: LeftTimeInformationBox,
   parameters: {
     layout: 'centered',
@@ -15,6 +15,15 @@ type Story = StoryObj<typeof LeftTimeInformationBox>;
 export const Default: Story = {
   args: {
     title: '나의 첫 스페이스',
-    openedAt: '2025-07-29T09:06:02.801Z',
+    leftTime: '23:59:59',
+    openDate: '2025-07-29T09:06:02.801Z',
+  },
+};
+
+export const OneDayLeft: Story = {
+  args: {
+    title: '내일 열리는 스페이스',
+    leftTime: '1일',
+    openDate: '2025-07-29T09:06:02.801Z',
   },
 };
