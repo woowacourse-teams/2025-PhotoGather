1d35759c70c3acf3208c6a82eaed24c8bd402b44
Hista <dtmdgus901@gmail.com>
2025-08-01 23:50:36 +0900

    refactor: 클래스 책임 분리

--- backend/forgather/src/main/java/com/forgather/domain/space/util/RandomCodeGenerator.java
+++ backend/forgather/src/main/java/com/forgather/domain/space/util/RandomCodeGenerator.java
@@ -10,8 +10,4 @@ public class RandomCodeGenerator {
     public String generate() {
         return UUID.randomUUID().toString().substring(0, 11).replace("-", "");
     }
-
-    public String generate(int count) {
-        return UUID.randomUUID().toString().substring(0, count).replace("-", "");
-    }
 }
--- backend/forgather/src/main/java/com/forgather/global/logging/TraceIdInterceptor.java
+++ backend/forgather/src/main/java/com/forgather/global/logging/TraceIdInterceptor.java
@@ -1,9 +1,9 @@
 package com.forgather.global.logging;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import com.forgather.domain.space.util.RandomCodeGenerator;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,12 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class TraceIdInterceptor implements HandlerInterceptor {
 
-    private final RandomCodeGenerator randomCodeGenerator;
+    private static final int TRACE_ID_LENGTH = 4;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
         Exception {
-        String traceId = randomCodeGenerator.generate(4);
+        String traceId = UUID.randomUUID().toString().substring(0, TRACE_ID_LENGTH).replace("-", "");
         request.setAttribute("com.forgather.traceId", traceId);
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
