943e4281d4aa90162750345b67d82aceab08a455
H0ngJu <hongju4385@gmail.com>
2025-08-01 14:06:01 +0900

    refactor: formatTime 제거 및 formatDate에서 date, time을 반환하도록 수정

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,3 +1,14 @@
-export const formatDate = (year: number, month: number, day: number) => {
-  return `${year}년 ${month}월 ${day}일`;
+export const formatDate = (rawDate: string) => {
+  const parsedDate = new Date(rawDate);
+
+  const formattedDate = `${parsedDate.getFullYear()}년 ${parsedDate.getMonth() + 1}월 ${parsedDate.getDate()}일`;
+  const formattedTime =
+    parsedDate.getHours() > 12
+      ? `오후 ${parsedDate.getHours() - 12}시 ${parsedDate.getMinutes()}분`
+      : `오전 ${parsedDate.getHours()}시 ${parsedDate.getMinutes()}분`;
+
+  return {
+    date: formattedDate,
+    time: formattedTime,
+  };
 };
