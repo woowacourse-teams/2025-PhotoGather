a64f049a86d50197867c26ad0f09f70f64cb84a6
ykmxxi <ykmxxi97@gmail.com>
2025-08-03 21:26:54 +0900

    docs: 사진 일괄 삭제 swagger 추가

--- backend/forgather/src/main/java/com/forgather/domain/space/controller/PhotoController.java
+++ backend/forgather/src/main/java/com/forgather/domain/space/controller/PhotoController.java
@@ -108,6 +108,7 @@ public class PhotoController {
     }
 
     @DeleteMapping
+    @Operation(summary = "사진 일괄 삭제", description = "특정 공간의 사진을 일괄 삭제합니다.")
     public ResponseEntity<Void> deleteAll(
         @PathVariable(name = "spaceCode") String spaceCode
     ) {
