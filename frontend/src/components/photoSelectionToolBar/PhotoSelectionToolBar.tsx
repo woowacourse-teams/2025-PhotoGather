11b89ffbca5c2915f689e685992eb85387da1b57
hanheel <hanhee630630@gmail.com>
2025-08-04 17:14:13 +0900

    docs: JSDoc 추가

--- frontend/src/components/photoSelectionToolBar/PhotoSelectionToolBar.tsx
+++ frontend/src/components/photoSelectionToolBar/PhotoSelectionToolBar.tsx
@@ -3,8 +3,11 @@ import HighlightText from '../@common/highlightText/HighlightText';
 import * as S from './PhotoSelectionToolBar.styles';
 
 interface PhotoSelectionToolBarProps {
+  /** 선택된 사진 개수 */
   selectedCount: number;
+  /** 삭제 버튼 클릭 시 실행할 함수 */
   onDelete: () => void;
+  /** 다운로드 버튼 클릭 시 실행할 함수 */
   onDownload: () => void;
 }
 
