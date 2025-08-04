190ea4f6787281690389ccd3e794021850713b36
H0ngJu <hongju4385@gmail.com>
2025-08-04 14:57:21 +0900

    refactor: 3항연산자 -> if 문으로 분기되도록 수정

--- frontend/src/utils/formatTimer.ts
+++ frontend/src/utils/formatTimer.ts
@@ -2,7 +2,6 @@ import type { Timer } from '../types/timer.type';
 
 export const formatTimer = (timer: Timer) => {
   const pad = (n: number) => n.toString().padStart(2, '0');
-  return timer.days > 0
-    ? `${timer.days}일`
-    : `${pad(timer.hours)}:${pad(timer.minutes)}:${pad(timer.seconds)}`;
+  if (timer.days > 0) return `${timer.days}일`;
+  return `${pad(timer.hours)}:${pad(timer.minutes)}:${pad(timer.seconds)}`;
 };
