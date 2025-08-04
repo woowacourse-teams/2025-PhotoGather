2571fd487aac90e59ff03daf0e54f49173f9812b
Hongju Lee <hongju4385@gmail.com>
2025-08-04 17:59:49 +0900

    feature: 타이머 훅 제작 (#226)
    * style: 불필요한 주석 삭제
    
    * feat: 현재시간 기준으로 남은 시간 계산하는 useTimer 훅 구현
    
    * refactor: formatTime 제거 및 formatDate에서 date, time을 반환하도록 수정
    
    * feat: 타이머 string으로 변환하는 유틸 함수 작성
    
    * style: 잘못된 주석 수정
    
    * refactor: interface명 수정
    
    * refactor: useTimer -> useLeftTimer, time -> leftTime으로 수정
    
    * refactor: 3항연산자 -> if 문으로 분기되도록 수정
    
    * feat: Date 객체로 파싱가능한지 확인하는 함수 작성 및 적용

--- frontend/src/components/leftTimeInformationBox/LeftTimeInformationBox.tsx
+++ frontend/src/components/leftTimeInformationBox/LeftTimeInformationBox.tsx
@@ -1,52 +1,28 @@
 import loadingImage from '@assets/images/loading.png';
-import { formatDate } from '../../utils/formatDate';
-import { formatTime } from '../../utils/formatTime';
 import HighlightText from '../@common/highlightText/HighlightText';
 import * as S from './LeftTimeInformationBox.styles';
 
-interface InformationConfirmBoxProps {
+interface OpenDate {
+  /** 오픈하는 날짜 */
+  date: string;
+  /** 오픈하는 시각 */
+  time: string;
+}
+
+interface LeftTimeInformationBoxProps {
   /** 스페이스 타이틀 */
   title: string;
   /** 오픈까지 남은 날짜와 시간 */
   leftTime: string;
   /** 오픈하는 날짜와 시간 */
-  openDate: string;
+  openDate: OpenDate;
 }
 
 const LeftTimeInformationBox = ({
   title,
   leftTime,
   openDate,
-}: InformationConfirmBoxProps) => {
-  // TODO: 코드 삭제
-  const date = new Date(openDate);
-  const today = new Date();
-
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  // TODO: 컴포넌트를 사용하는 페이지에서 이렇게 구현
-  const formattedDate = formatDate(year, month, day);
-  const formattedTime = formatTime(hours, minutes);
-
-  // TODO: hook으로 분리
-  const timeDiff = date.getTime() - today.getTime();
-  const totalSeconds = Math.floor(Math.abs(timeDiff) / 1000);
-  const totalHours = Math.floor(totalSeconds / (60 * 60)); // 1시간 = 3600초
-  const totalDays = Math.floor(totalHours / 24);
-  const hoursLeft = totalHours % 24;
-  const minutesLeft = Math.floor((totalSeconds % (60 * 60)) / 60);
-  const secondsLeft = totalSeconds % 60;
-
-  const dDay =
-    totalHours < 24
-      ? `${hoursLeft.toString().padStart(2, '0')}:${minutesLeft.toString().padStart(2, '0')}:${secondsLeft.toString().padStart(2, '0')}`
-      : `${totalDays}일`;
-
+}: LeftTimeInformationBoxProps) => {
   return (
     <S.Wrapper>
       <S.Title>{title}</S.Title>
@@ -58,14 +34,8 @@ const LeftTimeInformationBox = ({
       />
       <S.Icon src={loadingImage} alt="loading" />
       <HighlightText
-        text={formattedDate}
-        highlightTextArray={[formattedDate]}
-        highlightColorStyle="primary"
-        fontStyle="bodyLarge"
-      />
-      <HighlightText
-        text={`${formattedTime}에 열릴 예정이에요`}
-        highlightTextArray={[formattedTime]}
+        text={`${openDate.date}\n${openDate.time}에 열릴 예정이에요`}
+        highlightTextArray={[openDate.date, openDate.time]}
         highlightColorStyle="primary"
         fontStyle="bodyLarge"
       />
