0b9b98d1dd0481e8cb9317ddfe41bc3ee37ecd0d
Hanhee Lee <168459001+hanheel@users.noreply.github.com>
2025-08-05 16:38:54 +0900

    chore: sentry install 및 초기 설정 (#247)

--- frontend/src/pages/demo/DemoHome.tsx
+++ frontend/src/pages/demo/DemoHome.tsx
@@ -1,4 +1,5 @@
 import rocketImage from '@assets/images/rocket.png';
+import * as Sentry from '@sentry/react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/@common/buttons/button/Button';
 import { ROUTES } from '../../constants/routes';
@@ -6,7 +7,16 @@ import * as S from './DemoHome.styles';
 
 const DemoHome = () => {
   const navigate = useNavigate();
-
+  const testFunc = () => {
+    try {
+      throw new Error(
+        '축하합니다. 성공적으로 sentry 설정을 마치셨습니다 우하하',
+      );
+    } catch (err) {
+      Sentry.captureException(err);
+      Sentry.captureMessage('가짜 축하');
+    }
+  };
   return (
     <S.Wrapper>
       <S.Icon src={rocketImage} alt="데모 페이지 아이콘"></S.Icon>
@@ -19,6 +29,7 @@ const DemoHome = () => {
         text="(MANAGER) 스페이스 관리 페이지 이동"
         onClick={() => navigate(ROUTES.MANAGER.SPACE_HOME)}
       />
+      <Button text="테스트 에러 발생" onClick={testFunc} />
     </S.Wrapper>
   );
 };
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,85 +3,89 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const DotenvPlugin = require('dotenv-webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/@assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/@assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
       },
-      {
-        test: /\.(woff|woff2)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/fonts/[name][hash][ext]',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
         },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/images/[name].[hash][ext]',
+        {
+          test: /\.(woff|woff2)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/fonts/[name][hash][ext]',
+          },
         },
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              svgo: true,
-              titleProp: true,
-              ref: true,
-            },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/images/[name].[hash][ext]',
           },
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'static/media/[name].[hash].[ext]',
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                svgo: true,
+                titleProp: true,
+                ref: true,
+              },
             },
-          },
-        ],
-      },
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'static/media/[name].[hash].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new DotenvPlugin({
+        path: isProduction ? './.env.production' : './.env',
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        async: true,
+      }),
     ],
-  },
-  plugins: [
-    new DotenvPlugin({
-      path: './.env',
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: true,
-    }),
-  ],
-  devServer: {
-    static: './dist',
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
+    devServer: {
+      static: './dist',
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
 };
