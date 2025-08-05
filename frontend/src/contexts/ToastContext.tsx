023da09090c4d1055681e347d2b52c552e2a2995
H0ngJu <hongju4385@gmail.com>
2025-08-05 23:11:28 +0900

    feat: ToastProvider 구현

---
+++ frontend/src/contexts/ToastContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from 'react';
+import { ToastList } from '../components/@common/notification/toastList/ToastList';
+import type { Toast, ToastBase } from '../types/toast.type';
+
+interface ToastContextProps {
+  showToast: (options: ToastBase) => void;
+  removeToast: (id: number) => void;
+  toasts: Toast[];
+}
+
+export const ToastContext = createContext<ToastContextProps | undefined>(
+  undefined,
+);
+
+let toastId = 0;
+
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toasts, setToasts] = useState<Toast[]>([]);
+
+  const removeToast = (id: number) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  };
+
+  const showToast = ({ text, type = 'error', duration = 5000 }: ToastBase) => {
+    const id = toastId++;
+    setToasts((prev) => [...prev, { id, text, type, duration }]);
+
+    setTimeout(() => {
+      removeToast(id);
+    }, duration);
+  };
+
+  return (
+    <ToastContext.Provider value={{ showToast, removeToast, toasts }}>
+      {children}
+      <ToastList toasts={toasts} />
+    </ToastContext.Provider>
+  );
+};
