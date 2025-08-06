2d8a5b98aca5bb19d3163a1d1c21ccf480b52a5c
ykmxxi <ykmxxi97@gmail.com>
2025-08-06 23:07:22 +0900

    docs: Swagger 적용

--- backend/forgather/src/main/java/com/forgather/domain/space/dto/DeletePhotosRequest.java
+++ backend/forgather/src/main/java/com/forgather/domain/space/dto/DeletePhotosRequest.java
@@ -2,7 +2,10 @@ package com.forgather.domain.space.dto;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record DeletePhotosRequest(
+    @Schema(description = "삭제할 사진 ID 목록", example = "[1, 2, 3, 4]")
     List<Long> photoIds
 ) {
 }
