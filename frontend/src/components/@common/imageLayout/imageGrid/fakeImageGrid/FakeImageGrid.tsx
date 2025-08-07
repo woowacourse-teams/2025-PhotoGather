69a238bd1dc2bd4df40bbbb6d3b0651eee30025b
Minje Sung <minje813@dgu.ac.kr>
2025-08-08 02:13:38 +0900

    feat: FakeImageGrid, FakeImageElement 컴포넌트 작성

---
+++ frontend/src/components/@common/imageLayout/imageGrid/fakeImageGrid/FakeImageGrid.tsx
@@ -0,0 +1,22 @@
+import FakeImageElement from '../../imageElement/fakeImageElement/FakeImageElement';
+import * as S from '../ImageGrid.common.styles';
+
+interface FakeImageGridProps {
+  /** 표시할 이미지의 개수 */
+  photoLength: number;
+  /** 한 줄당 이미지 개수 */
+  rowImageAmount: number;
+}
+
+const FakeImageGrid = ({ photoLength, rowImageAmount }: FakeImageGridProps) => {
+  return (
+    <S.Wrapper $rowImageAmount={rowImageAmount}>
+      {Array.from({ length: photoLength }).map((_, index) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: 가짜 이미지는 index만 가지고 있음
+        <FakeImageElement key={index} />
+      ))}
+    </S.Wrapper>
+  );
+};
+
+export default FakeImageGrid;
