fe1872e0d234a580e093337626d5b5300b9992a1
Hongju Lee <hongju4385@gmail.com>
2025-08-07 16:27:13 +0900

    feature: 레이아웃 배경 별 추가 및 그라디언트 조정 (#271)
    * feat: layout 별 배경 추가
    
    * refactor: 별 위치 수정 및 고정
    
    * style: format:styles 적용
    
    * refactor: 별 개수 조정

--- frontend/src/components/layout/Layout.styles.ts
+++ frontend/src/components/layout/Layout.styles.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div<{ $isHighlightPage: boolean }>`
+  position: relative;
   margin: 0 auto;
   max-width: ${({ theme }) => theme.layout.width};
   width: 100%;
@@ -9,6 +10,6 @@ export const Container = styled.div<{ $isHighlightPage: boolean }>`
   min-height: 100dvh;
   background: ${({ theme, $isHighlightPage }) =>
     $isHighlightPage
-      ? `linear-gradient(180deg, ${theme.colors.gradient.start} 0%, ${theme.colors.gradient.end} 100%)`
+      ? `linear-gradient(120deg, #0f0c29 0%, rgb(84, 26, 176) 45%, #24243e 80%);`
       : theme.colors.white};
 `;
