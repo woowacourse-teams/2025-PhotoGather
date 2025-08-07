c66e3ee0f34027c795027396560d664c81eab022
H0ngJu <hongju4385@gmail.com>
2025-08-07 11:29:18 +0900

    refactor: 별 위치 수정 및 고정

--- frontend/src/components/layout/starField/StarField.styles.ts
+++ frontend/src/components/layout/starField/StarField.styles.ts
@@ -2,6 +2,15 @@ import FilledStar from '@assets/icons/star-filled.svg';
 import OutlinedStar from '@assets/icons/star-outlined.svg';
 import styled from '@emotion/styled';
 
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  pointer-events: none;
+`;
+
 export const StarContainer = styled.div<{
   top: number;
   left: number;
@@ -9,18 +18,18 @@ export const StarContainer = styled.div<{
   delay?: number;
   star?: 'outlined' | 'filled';
 }>`
-  position: fixed;
+  position: absolute;
   top: ${({ top }) => `${top}%`};
   left: ${({ left }) => `${left}%`};
   width: ${({ size }) => `${size}px`};
   height: ${({ size }) => `${size}px`};
   background: ${({ star }) => `
-  url(${star === 'filled' ? FilledStar : OutlinedStar}) no-repeat center / contain
-`};
+    url(${star === 'filled' ? FilledStar : OutlinedStar}) no-repeat center / contain
+  `};
   animation: spinAndScale 3s linear infinite;
   animation-delay: ${({ delay }) => `${delay || 0}s`};
-  pointer-events: none;
   opacity: 0.8;
+  pointer-events: none;
 
   @keyframes spinAndScale {
     0% {
--- frontend/src/components/layout/starField/StarField.tsx
+++ frontend/src/components/layout/starField/StarField.tsx
@@ -1,28 +1,27 @@
-import { StarContainer } from './StarField.styles';
+import * as S from './StarField.styles';
 
 const stars = [
-  { top: 5, left: 10, size: 30, delay: 0.5 },
-  { top: 12, left: 80, size: 20, delay: 1.2 },
-  { top: 18, left: 30, size: 10, delay: 2.1 },
-  { top: 25, left: 60, size: 20, delay: 0.8 },
-  { top: 33, left: 20, size: 13, delay: 1.7 },
-  { top: 38, left: 75, size: 12, delay: 0.4 },
-  { top: 42, left: 15, size: 8, delay: 2.5 },
-  { top: 47, left: 50, size: 9, delay: 1.0 },
-  { top: 53, left: 70, size: 22, delay: 0.9 },
-  { top: 60, left: 25, size: 30, delay: 1.3 },
-  { top: 66, left: 65, size: 29, delay: 2.8 },
-  { top: 72, left: 35, size: 25, delay: 0.6 },
-  { top: 78, left: 85, size: 15, delay: 1.1 },
-  { top: 84, left: 40, size: 19, delay: 2.0 },
-  { top: 90, left: 55, size: 43, delay: 1.6 },
+  { top: 5, left: 50, size: 30, delay: 0.5 },
+  { top: 10, left: 87, size: 20, delay: 1.2 },
+  { top: 98, left: 80, size: 10, delay: 2.1 },
+  { top: 20, left: 9, size: 20, delay: 0.8 },
+  { top: 2, left: 70, size: 13, delay: 1.7 },
+  { top: 38, left: 88, size: 12, delay: 0.4 },
+  { top: 42, left: 10, size: 22, delay: 2.5 },
+  { top: 2, left: 20, size: 9, delay: 1.0 },
+  { top: 95, left: 20, size: 22, delay: 0.9 },
+  { top: 77, left: 8, size: 30, delay: 1.3 },
+  { top: 83, left: 84, size: 29, delay: 2.8 },
+  { top: 14, left: 67, size: 25, delay: 0.6 },
+  { top: 96, left: 70, size: 15, delay: 1.1 },
+  { top: 96, left: 40, size: 43, delay: 1.6 },
 ];
 
 export const StarField = () => {
   return (
-    <>
+    <S.Wrapper>
       {stars.map((star, index) => (
-        <StarContainer
+        <S.StarContainer
           // biome-ignore lint/suspicious/noArrayIndexKey: false positive
           key={index}
           top={star.top}
@@ -32,6 +31,6 @@ export const StarField = () => {
           star={index % 2 === 0 ? 'filled' : 'outlined'}
         />
       ))}
-    </>
+    </S.Wrapper>
   );
 };
