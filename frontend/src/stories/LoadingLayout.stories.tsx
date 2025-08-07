387d9e74a90853cfedaa76c93743ad39b742677d
Hanhee Lee <168459001+hanheel@users.noreply.github.com>
2025-08-07 16:53:10 +0900

    feature: loading UI 제작 (#266)
    * feat: loading 컴포넌트 제작
    
    * feat: SpaceHome에 로딩창 연결
    
    * feat: imageUpload에 로딩창 연결
    
    * refactor: style 린트 설정
    
    * refactor: biome 린트 에러 해결
    
    * refactor: biome 린트 에러 해결
    
    * refactor: 코드 출처 주석 작성
    
    * feat: loading interface 수정
    
    icon 및 description을 함께 묶어 제작
    
    * fix: loadingContents interface에 맞게 page 수정
    
    * chore: merge develop

--- frontend/src/stories/LoadingLayout.stories.tsx
+++ frontend/src/stories/LoadingLayout.stories.tsx
@@ -12,17 +12,23 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    iconList: [
-      { src: rocketImage, alt: '데모 페이지 아이콘' },
-      { src: rocketImage, alt: '데모 페이지 아이콘' },
-      { src: rocketImage, alt: '데모 페이지 아이콘' },
-      { src: rocketImage, alt: '데모 페이지 아이콘' },
-    ],
-    descriptionList: [
-      '로딩 텍스트 1',
-      '로딩 텍스트 2',
-      '로딩 텍스트 3',
-      '로딩 텍스트 4',
+    loadingContents: [
+      {
+        icon: { src: rocketImage, alt: '데모 페이지 아이콘' },
+        description: '로딩 텍스트 1',
+      },
+      {
+        icon: { src: rocketImage, alt: '데모 페이지 아이콘' },
+        description: '로딩 텍스트 2',
+      },
+      {
+        icon: { src: rocketImage, alt: '데모 페이지 아이콘' },
+        description: '로딩 텍스트 3',
+      },
+      {
+        icon: { src: rocketImage, alt: '데모 페이지 아이콘' },
+        description: '로딩 텍스트 4',
+      },
     ],
     percentage: 30,
   },
