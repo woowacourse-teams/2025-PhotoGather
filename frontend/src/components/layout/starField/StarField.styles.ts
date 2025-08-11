b39ad6ae284c2e0f03a26eca988e930bcb004a4e
H0ngJu <hongju4385@gmail.com>
2025-08-11 21:01:05 +0900

    refactor: 별이 고정된 위치에 있지 않고 밀리는 문제 해결

--- frontend/src/components/layout/starField/StarField.styles.ts
+++ frontend/src/components/layout/starField/StarField.styles.ts
@@ -3,13 +3,26 @@ import OutlinedStar from '@assets/icons/star-outlined.svg';
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  position: fixed;
+  inset: 0;
+  pointer-events: none;
+  z-index: 0;
+`;
+
+export const CenterClip = styled.div`
   position: absolute;
   top: 0;
-  left: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: min(100%, ${({ theme }) => theme.layout.width});
+  height: 100%;
+  overflow: hidden;
+`;
+
+export const Canvas = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
-  pointer-events: none;
-  overflow: hidden;
 `;
 
 export const StarContainer = styled.div<{
