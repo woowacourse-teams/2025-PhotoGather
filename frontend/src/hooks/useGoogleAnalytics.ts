6e044c3c46813fc7089b975ecf6a895d683ae075
H0ngJu <hongju4385@gmail.com>
2025-08-12 19:17:31 +0900

    refactor: 오류 수정

--- frontend/src/hooks/useGoogleAnalytics.ts
+++ frontend/src/hooks/useGoogleAnalytics.ts
@@ -9,7 +9,7 @@ const useGoogleAnalytics = () => {
   //biome-ignore lint/correctness/useExhaustiveDependencies: GA4 초기화 코드는 첫 마운트시 실행
   useEffect(() => {
     /* 개발모드에서 GA 트래픽확인하기 위해서는 아래 코드 대신 사용 */
-    /* const isDev = process.env.ENVIRONMENT === 'development';
+    const isDev = process.env.ENVIRONMENT === 'development';
     if (!MEASUREMENT_ID) {
       console.log('🚫 GA4 측정 ID 없음 - 초기화 생략');
       return;
@@ -18,20 +18,24 @@ const useGoogleAnalytics = () => {
     if (isDev) {
       ReactGA.initialize(MEASUREMENT_ID, {
         // 타입에 맞는 필드만 전달 (원하면 testMode: true 도 가능)
-        gtagOptions: {},
+        gtagOptions: {
+          debug_mode: true,
+          send_page_view: false,
+        },
       });
 
       // DebugView 보장
-      ReactGA.gtag('config', MEASUREMENT_ID, { debug_mode: true });
-    } else ReactGA.initialize(MEASUREMENT_ID); */
+      // ReactGA.gtag('config', MEASUREMENT_ID, { debug_mode: true });
+      console.log(MEASUREMENT_ID, ReactGA.gtag);
+    } else ReactGA.initialize(MEASUREMENT_ID);
 
-    if (!MEASUREMENT_ID) {
-      console.log('🚫 로컬 환경 - GA4 초기화 건너뜀');
-      return;
-    }
+    // if (!MEASUREMENT_ID) {
+    //   console.log('🚫 로컬 환경 - GA4 초기화 건너뜀');
+    //   return;
+    // }
 
-    ReactGA.initialize(MEASUREMENT_ID);
-    console.log('✅ GA4 초기화 완료:', MEASUREMENT_ID);
+    // ReactGA.initialize(MEASUREMENT_ID);
+    // console.log('✅ GA4 초기화 완료:', MEASUREMENT_ID);
   }, []);
 
   //biome-ignore lint/correctness/useExhaustiveDependencies: GA4 페이지뷰 전송 코드는 페이지 변경 시 실행
