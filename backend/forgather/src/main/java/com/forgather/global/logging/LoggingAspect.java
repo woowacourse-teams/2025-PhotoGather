ccfac612cea2e2b01eaa2043bae8218075f02de5
kjyyjk <rlawnsdud920@gmail.com>
2025-08-14 00:14:14 +0900

    refactor: 로깅 과정 및 출력 형식 변경
    - Slf4j에서 제공하는 로그 빌더 패턴을 활용한다.

--- backend/forgather/src/main/java/com/forgather/global/logging/LoggingAspect.java
+++ backend/forgather/src/main/java/com/forgather/global/logging/LoggingAspect.java
@@ -1,10 +1,15 @@
 package com.forgather.global.logging;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,21 +19,31 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class LoggingAspect {
 
-    private final LogFormatter logFormatter;
+    private final HttpServletRequest request;
 
     @Around("@within(org.springframework.stereotype.Service) || "
         + "execution(* com.forgather.domain.space.service.AwsS3Cloud.*(..))")
     public Object logging(final ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodInformation = logFormatter.formatMethodInformation(joinPoint);
-        String requestInformation = logFormatter.formatRequestInformation();
-
         long startMillis = System.currentTimeMillis();
         Object result = joinPoint.proceed();
-        String durationInformation = logFormatter.formatDurationMillis(System.currentTimeMillis() - startMillis);
+        long durationMillis = System.currentTimeMillis() - startMillis;
 
-        log.info("{} {} {}",
-            methodInformation, durationInformation, requestInformation);
-
+        log.atInfo()
+            .addKeyValue("methodName", getMethodName(joinPoint))
+            .addKeyValue("methodParams", getMethodParams(joinPoint))
+            .addKeyValue("requestUri", request.getRequestURI())
+            .log("({}ms)", durationMillis);
         return result;
     }
+
+    public String getMethodName(JoinPoint joinPoint) {
+        return joinPoint.getSignature().toShortString();
+    }
+
+    public String getMethodParams(JoinPoint joinPoint) {
+        return Arrays.stream(joinPoint.getArgs())
+            .map(arg -> arg == null ? "null" : arg.toString())
+            .collect(Collectors.joining(", "));
+    }
+
 }
--- backend/forgather/src/main/java/com/forgather/global/logging/LoggingInterceptor.java
+++ backend/forgather/src/main/java/com/forgather/global/logging/LoggingInterceptor.java
@@ -1,5 +1,6 @@
 package com.forgather.global.logging;
 
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,33 +20,59 @@ public class LoggingInterceptor implements HandlerInterceptor {
     private static final int TRACE_ID_LENGTH = 8;
 
     private final RandomCodeGenerator randomCodeGenerator;
-    private final Logger logger;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // TODO 클라이언트에게서 traceId를 받기
         String traceId = randomCodeGenerator.generate(TRACE_ID_LENGTH);
         request.setAttribute("com.forgather.traceId", traceId);
         request.setAttribute("com.forgather.startTime", System.currentTimeMillis());
 
-        logger.log()
-            .event("REQUEST")
-            .value("httpMethod", request.getMethod())
-            .info();
+        // 해당 쓰레드에서 발생하는 모든 로그에 대해 자동으로 추가됨
+        MDC.put("traceId", traceId);
+        MDC.put("event", "REQUEST");
+        log.atInfo()
+            .addKeyValue("httpMethod", request.getMethod())
+            .addKeyValue("requestUri", request.getRequestURI())
+            .addKeyValue("ip", getClientIp(request))
+            .addKeyValue("userAgent", getUserAgent(request))
+            .log();
+        MDC.remove("event");
         return true;
+    }
+
+    private String getClientIp(HttpServletRequest request) {
+        String forwarded = request.getHeader("X-Forwarded-For");
+        if (forwarded != null) {
+            return forwarded.split(",")[0]; // 여러 프록시 거친 경우 첫 IP가 실제 클라이언트
+        }
+        return request.getRemoteAddr();
+    }
+
+    private String getUserAgent(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent");
+        if (userAgent == null || userAgent.isEmpty()) {
+            return "UnknownUserAgent";
+        }
+        return userAgent;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
         Exception exception) {
-        HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
-        String event = httpStatus.isError() ? "ERROR_RESPONSE" : "RESPONSE";
-        Long startTime = (Long)request.getAttribute("com.forgather.startTime");
+        Long startTime = (Long) request.getAttribute("com.forgather.startTime");
         long durationMillis = (startTime != null) ? (System.currentTimeMillis() - startTime) : -1;
 
-        logger.log()
-            .event(event)
-            .value("durationMillis", durationMillis)
-            .value("statusCode", httpStatus.value())
-            .info();
+        MDC.put("event", getResponseEvent(response));
+        log.atInfo()
+            .log("({}ms)", durationMillis);
+
+        // 쓰레드 재활용을 대비한 초기화
+        MDC.clear();
+    }
+
+    private String getResponseEvent(HttpServletResponse response) {
+        HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
+        return httpStatus.isError() ? "ERROR_RESPONSE" : "RESPONSE";
     }
 }
