aca887025ed1464d868cada9e2b88c799f803b62
Hongju Lee <hongju4385@gmail.com>
2025-08-13 11:03:32 +0900

    fix: 토스트 개선, 헤더 문구 변경 및 별 배경 구조 개선 (#320)
    * refactor: 스페이스 헤더 문구 수정
    
    * refactor: 기본 토스트 지속시간을 2초로 수정
    
    * refactor: 토스트 크기 및 색상 수정
    
    * refactor: 별이 고정된 위치에 있지 않고 밀리는 문제 해결
    
    * refactor: 지속시간 1500으로 수정

--- frontend/src/components/@common/notification/toastList/ToastList.styles.ts
+++ frontend/src/components/@common/notification/toastList/ToastList.styles.ts
@@ -3,8 +3,7 @@ import type { ToastListPosition } from '../../../../types/toast.type';
 
 export const ToastList = styled.div<{ $position: ToastListPosition }>`
   position: fixed;
-
-  ${({ $position }) => ($position === 'top' ? 'top: 40px;' : 'bottom: 40px;')}
+  ${({ $position }) => ($position === 'top' ? 'top: 100px;' : 'bottom: 100px;')}
 
   width: 100%;
   display: flex;
