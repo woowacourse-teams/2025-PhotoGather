cb256f4eadbce5474c1a08ebd8734629c146c69f
ykmxxi <ykmxxi97@gmail.com>
2025-08-17 15:11:21 +0900

    test: 서명된 URL 발급 테스트 추가
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>

--- backend/forgather/src/test/java/com/forgather/acceptance/AcceptanceTest.java
+++ backend/forgather/src/test/java/com/forgather/acceptance/AcceptanceTest.java
@@ -1,7 +1,6 @@
 package com.forgather.acceptance;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
