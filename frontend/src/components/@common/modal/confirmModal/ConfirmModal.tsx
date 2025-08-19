0f3efe8a912a2ce462d9ddaf2094d131aaa80d06
ShinjungOh <tlswjd9260@gmail.com>
2025-08-19 19:44:53 +0900

    refactor: ConfirmModal에 icon, title Props 추가 및 스토리북 업데이트

--- frontend/src/components/@common/modal/confirmModal/ConfirmModal.tsx
+++ frontend/src/components/@common/modal/confirmModal/ConfirmModal.tsx
@@ -4,21 +4,30 @@ import BaseModal from '../baseModal/BaseModal';
 import * as S from './ConfirmModal.styles';
 
 interface ConfirmModalProps extends BaseModalProps {
-  /** 모달에 표시할 이미지 URL */
+  /** 모달에 표시할 아이콘 */
+  icon?: React.ReactNode;
+  /** 모달에 표시할 이미지 */
   image?: string;
+  /** 모달 제목 */
+  title: string;
   /** 확인 메시지 */
   description?: string;
   /** 확인 버튼 텍스트 */
   confirmText?: string;
   /** 취소 버튼 텍스트 */
   cancelText?: string;
+  /** 모달 모드 */
+  mode?: 'default' | 'error';
 }
 
 const ConfirmModal = ({
+  icon,
   image,
-  description = '계속 진행할까요?',
+  title = '계속 진행할까요?',
+  description,
   confirmText = '확인',
   cancelText = '취소',
+  mode = 'default',
   onClose,
   onSubmit,
 }: ConfirmModalProps) => {
@@ -32,12 +41,20 @@ const ConfirmModal = ({
 
   return (
     <BaseModal>
+      {icon && <S.IconContainer>{icon}</S.IconContainer>}
       {image && (
-        <S.IconContainer>
+        <S.ImageContainer>
           <S.Icon src={image} alt={description} />
-        </S.IconContainer>
+        </S.ImageContainer>
       )}
-      <S.Description>{description}</S.Description>
+      <S.TextContainer>
+        <S.Title>{title}</S.Title>
+        {description ? (
+          <S.Description $isError={mode === 'error'}>
+            {description}
+          </S.Description>
+        ) : null}
+      </S.TextContainer>
       <S.ButtonContainer>
         <Button text={cancelText} variant="secondary" onClick={handleCancel} />
         <Button text={confirmText} variant="primary" onClick={handleConfirm} />
