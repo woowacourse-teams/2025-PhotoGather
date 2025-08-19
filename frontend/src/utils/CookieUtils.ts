c4df7c81e1a645c44fc8cf0928a4b44e4182a1de
Minje Sung <minje813@dgu.ac.kr>
2025-08-20 01:39:54 +0900

    feat: Cookie Delete 함수 작성

--- frontend/src/utils/CookieUtils.ts
+++ frontend/src/utils/CookieUtils.ts
@@ -41,4 +41,26 @@ export const CookieUtils = {
       document.cookie = cookieStr;
     }
   },
+
+  delete: async (
+    name: string,
+    options: {
+      path?: string;
+      domain?: string;
+    } = {},
+  ) => {
+    if ('cookieStore' in window) {
+      try {
+        await window.cookieStore.delete({ name });
+      } catch (error) {
+        console.error(`Failed to delete cookie "${name}":`, error);
+      }
+    } else {
+      let cookieStr = `${encodeURIComponent(name)}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+      if (options.path) cookieStr += `; path=${options.path}`;
+      if (options.domain) cookieStr += `; domain=${options.domain}`;
+      // biome-ignore lint/suspicious/noDocumentCookie: cookieStore를 지원하지 않는 레거시 브라우저를 위한 대체(fallback) 로직
+      document.cookie = cookieStr;
+    }
+  },
 };
