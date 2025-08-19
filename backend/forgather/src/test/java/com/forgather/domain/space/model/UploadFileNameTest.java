913b45e89419443bec8e95fab8bdd86ba8dd0b7a
posty <ykmxxi97@gmail.com>
2025-08-19 18:12:28 +0900

    feature: presigned-url 방식으로 사진 업로드 추가 구현 (#393)
    * feat: 요청한 갯수만큼 서명된 URL 발급
    
    * refactor: 서명된 URL 발급 로직 리팩터링 및 테스트
    
    * feat: 사진의 원본 이름 저장
    
    * feat: 업로드 된 사진 정보 DB 저장
    
    * feat: 확장자 별 url 발급
    
    * feat: 업로드 파일명 검증
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * feat: 업로드에 서명된 URL 적용
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * refactor: 사진 인수 테스트에 원본 이름 추가
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * feat: photo table 컬럼(원본 이름) 추가
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * test: 서명된 URL 발급 테스트 추가
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * docs: 스웨거 예시 수정
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * chore: import style 통일
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>
    
    * chore: 기존 업로드 API deprecated 처리
    
    * refactor: 파일 경로 생성기를 유틸로 이동
    
    * refactor: 업로드 서명된 URL 발급 API 엔드포인트 수정
    
    * refactor: 업로드 멀티파트 로직 업로드 서비스로 이동
    
    * feat: 확장자 존재 여부 판별에 대소문자 구분 없이 비교
    
    ---------
    
    Co-authored-by: kjyyjk <rlawnsdud920@gmail.com>

---
+++ backend/forgather/src/test/java/com/forgather/domain/space/model/UploadFileNameTest.java
@@ -0,0 +1,20 @@
+package com.forgather.domain.space.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class UploadFileNameTest {
+
+    @DisplayName("파일명이 유효하지 않으면 예외를 발생한다.")
+    @NullAndEmptySource
+    @ValueSource(strings = {".jpg", " .jpg", "name", "name.123", "/name.jpg"})
+    @ParameterizedTest
+    void validateUploadFileName(String invalidName) {
+        assertThatThrownBy(() -> new UploadFileName(invalidName))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
