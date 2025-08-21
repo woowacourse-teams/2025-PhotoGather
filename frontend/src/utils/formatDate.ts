c559d57f75f51ecf6196cdb7f6477bd01e192a83
H0ngJu <hongju4385@gmail.com>
2025-08-22 01:38:57 +0900

    refactor: formatDate에 padTwoDigit 함수 추출 및 자정인 경우 오전 12시로 표시되도록 수정

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -13,14 +13,22 @@ export const formatDate = (rawDate: string) => {
   }
 
   const parsedDate = new Date(rawDate);
-  const formattedDate = `${parsedDate.getFullYear()}년 ${parsedDate.getMonth() + 1}월 ${parsedDate.getDate()}일`;
+  const parsedMonth = padTwoDigits(parsedDate.getMonth() + 1);
+  const parsedDay = padTwoDigits(parsedDate.getDate());
+
+  const hour = parsedDate.getHours();
+  const parsedMinute = padTwoDigits(parsedDate.getMinutes());
+  const parsedHour = padTwoDigits(hour % 12 === 0 ? 12 : hour % 12);
+  const formattedDate = `${parsedDate.getFullYear()}년 ${parsedMonth}월 ${parsedDay}일`;
   const formattedTime =
     parsedDate.getHours() > 12
-      ? `오후 ${parsedDate.getHours() - 12}시 ${parsedDate.getMinutes()}분`
-      : `오전 ${parsedDate.getHours()}시 ${parsedDate.getMinutes()}분`;
+      ? `오후 ${parsedHour}시 ${parsedMinute}분`
+      : `오전 ${parsedHour}시 ${parsedMinute}분`;
 
   return {
     date: formattedDate,
     time: formattedTime,
   };
 };
+
+const padTwoDigits = (num: number) => String(num).padStart(2, '0');
