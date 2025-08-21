6f4c8746260664d3cbcd4c819058d5da5cb20449
hanheel <hanhee630630@gmail.com>
2025-08-21 18:22:36 +0900

    fix: 다운로딩 중 로딩 UI 삭제 및 다운로드 풀기

--- frontend/src/hooks/useDownload.ts
+++ frontend/src/hooks/useDownload.ts
@@ -109,11 +109,14 @@ const useDownload = ({
         setTotalProgress(downloadUrls.length);
         await downloadAsZip(downloadUrls);
       },
-      errorActions: ['toast'],
+      errorActions: ['toast', 'afterAction'],
       context: {
         toast: {
           text: '다운로드에 실패했습니다. 다시 시도해 주세요.',
           type: 'error',
+        },
+        afterAction: {
+          action: () => setIsDownloading(false),
         },
       },
     });
