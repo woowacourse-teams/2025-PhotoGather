600c7c75751ca434aadf66d82ce3489a0c11e879
Luna <97431021+ShinjungOh@users.noreply.github.com>
2025-08-21 22:11:41 +0900

    fix: 헤더 분기처리 및 프로필 이미지 설정 (#493)
    * refactor: Header에 mode 제거하고 location을 이용해 분기처리
    
    * feat: 메인 페이지에서 헤더 프로틸 보이지 않도록 처리
    
    * feat: default 프로필 이미지 설정
    
    * refactor: 마이페이지 필터링 칩 크기 키우기
    
    * refactor: 랜딩페이지에서 로그인 여부에 따른 헤더 UI 변경

--- frontend/src/types/route.type.ts
+++ frontend/src/types/route.type.ts
@@ -1,11 +1,9 @@
 import type { RouteObject } from 'react-router-dom';
-import type { HeaderMode } from '../components/layout/global/header/Header';
 
 export type RouteHandle = {
   header?: boolean;
   starField?: boolean;
   highlight?: boolean;
-  headerMode?: HeaderMode;
 };
 
 export type AppRouteObject = RouteObject & {
