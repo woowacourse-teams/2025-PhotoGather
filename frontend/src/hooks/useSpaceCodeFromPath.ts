cb15c02f06331b46eee1b454f7144aa6302a9c6f
hanheel <hanhee630630@gmail.com>
2025-08-22 13:25:47 +0900

    fix: 라우터 수정

--- frontend/src/hooks/useSpaceCodeFromPath.ts
+++ frontend/src/hooks/useSpaceCodeFromPath.ts
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 const useSpaceCodeFromPath = () => {
   const { pathname } = useLocation();
 
-  const match = pathname.match(/^\/(upload|space)\/([^/]+)/);
+  const match = pathname.match(/\/(image-upload|space)\/([^/]+)/);
 
   const spaceCode = match ? match[2] : null;
 
