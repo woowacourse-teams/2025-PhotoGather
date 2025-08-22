a91dbb7cb3f8f828943b71c7640006bf144904f1
hanheel <hanhee630630@gmail.com>
2025-08-22 12:28:36 +0900

    refactor: 버튼 텍스트 수정

--- frontend/src/pages/error/OpenBrowser.tsx
+++ frontend/src/pages/error/OpenBrowser.tsx
@@ -7,7 +7,7 @@ const OpenBrowserPage = () => {
       image={donutImage}
       title={'Forgather로 이동'}
       description={'하단 버튼을 눌러 이동해 주세요'}
-      buttonText={'Forgather로 이동'}
+      buttonText={'브라우저에서 열기'}
       highlightWords={['Forgather']}
       onButtonClick={() => {
         window.open(process.env.BASE_URL, '_blank');
