61b95372cf32cf21d61067558f9b55fac71178e7
hanheel <hanhee630630@gmail.com>
2025-08-28 19:56:39 +0900

    fix: navigate 위치 수정

--- frontend/src/hooks/@common/useInAppRedirect.ts
+++ frontend/src/hooks/@common/useInAppRedirect.ts
@@ -12,10 +12,12 @@ const useInAppRedirect = () => {
   const isInAppBrowserAllowPage = current?.handle?.isInAppBrowserAllow;
 
   const redirectInKakaoBrowser = (targetUrl: string) => {
+    navigate(ROUTES.IN_APP_BROWSER);
     window.location.href = `kakaotalk://web/openExternal?url=${encodeURIComponent(targetUrl)}`;
   };
 
   const redirectInInAppBrowser = (targetUrl: string) => {
+    navigate(ROUTES.IN_APP_BROWSER);
     if (checkIsIos()) {
       window.location.href = `x-safari-${targetUrl}`;
     } else {
@@ -33,6 +35,7 @@ const useInAppRedirect = () => {
   };
 
   const redirectInLineBrowser = (targetUrl: string) => {
+    navigate(ROUTES.IN_APP_BROWSER);
     if (location.indexOf('?') !== -1) {
       window.location.href = `${targetUrl}&openExternalBrowser=1`;
     } else {
@@ -50,7 +53,6 @@ const useInAppRedirect = () => {
     const isInAppBrowser = /(instagram|twitter)/.test(userAgent);
 
     const targetUrl = `${process.env.DOMAIN}${location}`;
-    navigate(ROUTES.IN_APP_BROWSER);
 
     if (isKakaoBrowser && !isInAppBrowserAllowPage) {
       redirectInKakaoBrowser(targetUrl);
