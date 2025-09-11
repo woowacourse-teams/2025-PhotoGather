1c596fcf1da910afd207924f66f9169592318239
H0ngJu <hongju4385@gmail.com>
2025-09-11 13:05:11 +0900

    refactor: 이미지 썸네일 파싱 주소 수정

--- frontend/src/utils/buildImageUrl.ts
+++ frontend/src/utils/buildImageUrl.ts
@@ -3,12 +3,7 @@ export const buildThumbnailUrl = (
   fileName: string,
   preset: string,
 ) => {
-  const basePath =
-    process.env.ENVIRONMENT === 'production'
-      ? 'photogather/contents'
-      : 'photogather/dev/contents';
-
-  return `${process.env.IMAGE_BASE_URL}/${basePath}/${spaceCode}/thumbnails/${fileName}_${preset}.webp`;
+  return `${process.env.IMAGE_BASE_URL}/${spaceCode}/thumbnails/${fileName}_${preset}.webp`;
 };
 
 export const buildOriginalImageUrl = (fileName: string) => {
