98979aaf5ac95acc96d2a390d545a858419ea318
Minje Sung <minje813@dgu.ac.kr>
2025-09-18 12:07:10 +0900

    feat: toggle 여부 인터페이스 추가

--- frontend/src/components/@common/toggle/Toggle.tsx
+++ frontend/src/components/@common/toggle/Toggle.tsx
@@ -1,9 +1,19 @@
 import * as S from './Toggle.styles';
 
-const Toggle = () => {
+interface ToggleProps {
+  /** 토글 여부 */
+  isToggle: boolean;
+  /** 클릭 시 실행할 함수 */
+  onToggleClick: () => void;
+}
+
+const Toggle = ({
+  isToggle,
+  onToggleClick: handleToggleClick,
+}: ToggleProps) => {
   return (
-    <S.Wrapper>
-      <p>토글임</p>
+    <S.Wrapper onClick={handleToggleClick}>
+      {isToggle ? <p>토글됨</p> : <p>토글안됨</p>}
     </S.Wrapper>
   );
 };
--- frontend/src/stories/Toggle.stories.tsx
+++ frontend/src/stories/Toggle.stories.tsx
@@ -1,9 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useState } from 'react';
 import Toggle from '../components/@common/toggle/Toggle';
 
 const meta: Meta<typeof Toggle> = {
   title: 'Components/Toggle',
   component: Toggle,
+  render: () => {
+    const [isToggle, setIsToggle] = useState(false);
+    return (
+      <Toggle
+        isToggle={isToggle}
+        onToggleClick={() => setIsToggle((prev) => !prev)}
+      />
+    );
+  },
 };
 export default meta;
 
