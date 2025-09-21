2537d117e0280bb2a836ab5c0caccbdb9e8270fa
hanheel <hanhee630630@gmail.com>
2025-09-22 03:04:42 +0900

    fix: 로그인하지 않은 사용자에 대한 대응 추가
    1. PRIVATE + 로그인 하지 않은 사용자 접근 = 똑같이 막기
    2. PUBLIC + 로그인 하지 않은 사용자 접근 = access를 true로 설정

--- frontend/src/hooks/domain/useSpaceAccess.ts
+++ frontend/src/hooks/domain/useSpaceAccess.ts
@@ -15,6 +15,29 @@ const useSpaceAccess = ({ spaceHostId, spaceType }: UseSpaceAccessProps) => {
   const [accessLoadingState, setAccessLoadingState] =
     useState<LoadingStateType>('pending');
 
+  const checkNoAuthAccess = async () => {
+    if (spaceType === 'PUBLIC') {
+      setHasAccess(true);
+      setAccessLoadingState('success');
+      return;
+    }
+    setHasAccess(false);
+    setAccessLoadingState('success');
+  };
+
+  const checkAuthAndAccess = async (visitorId: number) => {
+    if (spaceType === 'PUBLIC') {
+      setHasAccess(true);
+      setAccessLoadingState('success');
+      setHostId(visitorId);
+      return;
+    }
+    setHasAccess(visitorId === spaceHostId);
+    setHostId(visitorId);
+    setAccessLoadingState('success');
+  };
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: contextApI로 대체 예정
   useEffect(() => {
     if (!spaceHostId) {
       setHasAccess(false);
@@ -26,19 +49,13 @@ const useSpaceAccess = ({ spaceHostId, spaceType }: UseSpaceAccessProps) => {
     const fetchAccess = async () => {
       try {
         await authService.status().then((res) => {
-          if (spaceType === 'PUBLIC') {
-            setHasAccess(true);
-            setAccessLoadingState('success');
-            setHostId(res.data?.id);
-            return;
-          }
-          setHasAccess(res.data?.id === spaceHostId);
-          setHostId(res.data?.id);
-          setAccessLoadingState('success');
+          if (!res.data) return;
+          checkAuthAndAccess(res.data.id);
         });
       } catch (error) {
         console.error(`접근 권한 확인 실패 : ${error}`);
-        setAccessLoadingState('error');
+        // TODO : catch 단을 context에 hostId가 없을 경우로 변경 (로그인 하지 않은 사용자)
+        checkNoAuthAccess();
       }
     };
     fetchAccess();
