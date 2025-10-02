0a98abba3688bc28461f2bc8f87ac71e9d2c146c
ykmxxi <ykmxxi97@gmail.com>
2025-10-02 11:43:19 +0900

    feat: GuestBookCardPhoto 엔티티 구현

--- backend/forgather/src/main/java/com/forgather/v2/entity/GuestBookCardPhoto.java
+++ backend/forgather/src/main/java/com/forgather/v2/entity/GuestBookCardPhoto.java
@@ -1,6 +1,9 @@
 package com.forgather.v2.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GuestBookCardPhoto extends Photo {
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guest_book_card_id", nullable = false)
+    private GuestBookCard guestBookCard;
 }
