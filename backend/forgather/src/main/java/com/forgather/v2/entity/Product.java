b52ce7e0043e6901a806db08a13240107804d4de
ykmxxi <ykmxxi97@gmail.com>
2025-10-02 11:41:31 +0900

    feat: 엔티티에 JoinColumn 추가

--- backend/forgather/src/main/java/com/forgather/v2/entity/Product.java
+++ backend/forgather/src/main/java/com/forgather/v2/entity/Product.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +24,7 @@ public class Product extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "space_id", nullable = false)
     private Space space;
 
     @Column(name = "title")
--- backend/forgather/src/main/java/com/forgather/v2/entity/ProductPhoto.java
+++ backend/forgather/src/main/java/com/forgather/v2/entity/ProductPhoto.java
@@ -3,6 +3,7 @@ package com.forgather.v2.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class ProductPhoto extends Photo {
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     @Column(name = "sort_order", nullable = false)
