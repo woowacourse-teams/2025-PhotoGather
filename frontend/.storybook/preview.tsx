770093b3570ac93b7a1155cb7b926d54ca5c2062
Minje Sung <minje813@dgu.ac.kr>
2025-10-02 17:36:14 +0900

    chore: storybook 설치

---
+++ frontend/.storybook/preview.tsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from '@emotion/react';
+import type { Decorator, Preview } from '@storybook/react-vite';
+import GlobalStyles from '../src/styles/GlobalStyle';
+import { theme } from '../src/styles/theme';
+
+const withGlobalStyle: Decorator = (Story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Story />
+  </ThemeProvider>
+);
+
+const preview: Preview = {
+  decorators: [withGlobalStyle],
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'padded',
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
