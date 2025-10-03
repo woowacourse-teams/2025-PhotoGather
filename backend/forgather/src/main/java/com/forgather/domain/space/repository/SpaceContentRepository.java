3bb3c7197ccf8b0a77fa71ce9a63b2301fc27519
ykmxxi <ykmxxi97@gmail.com>
2025-10-03 19:47:32 +0900

    delete: SpaceContent 삭제

--- backend/forgather/src/main/java/com/forgather/domain/space/repository/SpaceContentRepository.java
+++
@@ -1,4 +0,0 @@
-package com.forgather.domain.space.repository;
-
-public interface SpaceContentRepository {
-}
--- backend/forgather/src/main/java/com/forgather/domain/space/repository/jpa/SpaceContentJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.forgather.domain.space.repository.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.forgather.domain.space.model.SpaceContent;
-import com.forgather.domain.space.repository.SpaceContentRepository;
-
-public interface SpaceContentJpaRepository extends JpaRepository<SpaceContent, Long>, SpaceContentRepository {
-}
