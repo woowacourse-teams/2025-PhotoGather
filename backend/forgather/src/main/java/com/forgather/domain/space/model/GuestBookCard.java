fd9852bf4dd1628dcca54176eb54bd04e38f550d
ykmxxi <ykmxxi97@gmail.com>
2025-10-03 19:48:14 +0900

    refactor: getter 추가

--- backend/forgather/src/main/java/com/forgather/domain/space/model/GuestBookCard.java
+++ backend/forgather/src/main/java/com/forgather/domain/space/model/GuestBookCard.java
@@ -13,8 +13,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GuestBookCard extends BaseTimeEntity {
