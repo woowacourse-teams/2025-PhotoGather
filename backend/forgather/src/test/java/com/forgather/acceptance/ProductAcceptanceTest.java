4f02905f8e9a59a0465a7d1490f210c3e28d0660
kjyyjk <rlawnsdud920@gmail.com>
2025-10-05 01:42:34 +0900

    test: 작품 수정 api 통합 테스트

--- backend/forgather/src/test/java/com/forgather/acceptance/ProductAcceptanceTest.java
+++ backend/forgather/src/test/java/com/forgather/acceptance/ProductAcceptanceTest.java
@@ -10,15 +10,19 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.forgather.domain.product.dto.ProductResponse;
 import com.forgather.domain.product.dto.RegisterProductPhotoRequest;
 import com.forgather.domain.product.dto.RegisterProductRequest;
+import com.forgather.domain.product.dto.UpdateProductRequest;
 import com.forgather.domain.space.model.Space;
 import com.forgather.domain.space.repository.SpaceRepository;
+import com.forgather.domain.upload.AwsS3Cloud;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -32,7 +36,22 @@ public class ProductAcceptanceTest extends AcceptanceTest {
     @Autowired
     private SpaceRepository spaceRepository;
 
+    @MockitoBean
+    private AwsS3Cloud awsS3Cloud;
+
     private Space space;
+
+    private RegisterProductRequest registerRequest = new RegisterProductRequest(
+        "title",
+        "category",
+        "authorName",
+        "description",
+        List.of(
+            new RegisterProductPhotoRequest("photo1", "path1", 1024),
+            new RegisterProductPhotoRequest("photo2", "path2", 2048),
+            new RegisterProductPhotoRequest("photo3", "path3", 4096)
+        )
+    );
 
     @BeforeEach
     void setUp() {
@@ -45,29 +64,10 @@ public class ProductAcceptanceTest extends AcceptanceTest {
     @Test
     public void get() {
         // given
-        RegisterProductRequest request = new RegisterProductRequest(
-            "title",
-            "category",
-            "authorName",
-            "description",
-            List.of(
-                new RegisterProductPhotoRequest("photo1", "path1", 1024),
-                new RegisterProductPhotoRequest("photo2", "path2", 2048),
-                new RegisterProductPhotoRequest("photo3", "path3", 4096)
-            )
-        );
-
-        RestAssuredMockMvc.given()
-            .body(request)
-            .contentType(ContentType.JSON)
-            .accept(ContentType.JSON)
-            .when()
-            .post("/spaces/%s/products".formatted(space.getCode()))
-            .then()
-            .statusCode(201);
+        ProductResponse registerResponse = registerProduct();
 
         // when
-        ProductResponse response = RestAssuredMockMvc.given()
+        ProductResponse result = RestAssuredMockMvc.given()
             .accept(ContentType.JSON)
             .when()
             .get("/spaces/%s/products".formatted(space.getCode()))
@@ -79,17 +79,17 @@ public class ProductAcceptanceTest extends AcceptanceTest {
 
         // then
         assertAll(
-            () -> assertThat(response.id()).isNotNull(),
-            () -> assertThat(response.title()).isEqualTo(request.title()),
-            () -> assertThat(response.category()).isEqualTo(request.category()),
-            () -> assertThat(response.authorName()).isEqualTo(request.authorName()),
-            () -> assertThat(response.description()).isEqualTo(request.description()),
-            () -> assertThat(response.photos().get(0).originalName()).isEqualTo("photo1"),
-            () -> assertThat(response.photos().get(0).order()).isEqualTo(1),
-            () -> assertThat(response.photos().get(1).originalName()).isEqualTo("photo2"),
-            () -> assertThat(response.photos().get(1).order()).isEqualTo(2),
-            () -> assertThat(response.photos().get(2).originalName()).isEqualTo("photo3"),
-            () -> assertThat(response.photos().get(2).order()).isEqualTo(3)
+            () -> assertThat(result.id()).isNotNull(),
+            () -> assertThat(result.title()).isEqualTo(registerResponse.title()),
+            () -> assertThat(result.category()).isEqualTo(registerResponse.category()),
+            () -> assertThat(result.authorName()).isEqualTo(registerResponse.authorName()),
+            () -> assertThat(result.description()).isEqualTo(registerResponse.description()),
+            () -> assertThat(result.photos().get(0).originalName()).isEqualTo("photo1"),
+            () -> assertThat(result.photos().get(0).order()).isEqualTo(1),
+            () -> assertThat(result.photos().get(1).originalName()).isEqualTo("photo2"),
+            () -> assertThat(result.photos().get(1).order()).isEqualTo(2),
+            () -> assertThat(result.photos().get(2).originalName()).isEqualTo("photo3"),
+            () -> assertThat(result.photos().get(2).order()).isEqualTo(3)
         );
     }
 
@@ -109,22 +109,9 @@ public class ProductAcceptanceTest extends AcceptanceTest {
     @DisplayName("작품 등록")
     @Test
     public void register() {
-        // given
-        RegisterProductRequest request = new RegisterProductRequest(
-            "title",
-            "category",
-            "authorName",
-            "description",
-            List.of(
-                new RegisterProductPhotoRequest("photo1", "path1", 1024),
-                new RegisterProductPhotoRequest("photo2", "path2", 2048),
-                new RegisterProductPhotoRequest("photo3", "path3", 4096)
-            )
-        );
-
         // when
         ProductResponse response = RestAssuredMockMvc.given()
-            .body(request)
+            .body(registerRequest)
             .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
             .when()
@@ -138,13 +125,16 @@ public class ProductAcceptanceTest extends AcceptanceTest {
         // then
         assertAll(
             () -> assertThat(response.id()).isNotNull(),
-            () -> assertThat(response.title()).isEqualTo(request.title()),
-            () -> assertThat(response.category()).isEqualTo(request.category()),
-            () -> assertThat(response.authorName()).isEqualTo(request.authorName()),
-            () -> assertThat(response.description()).isEqualTo(request.description()),
+            () -> assertThat(response.title()).isEqualTo(registerRequest.title()),
+            () -> assertThat(response.category()).isEqualTo(registerRequest.category()),
+            () -> assertThat(response.authorName()).isEqualTo(registerRequest.authorName()),
+            () -> assertThat(response.description()).isEqualTo(registerRequest.description()),
             () -> assertThat(response.photos().get(0).originalName()).isEqualTo("photo1"),
+            () -> assertThat(response.photos().get(0).order()).isEqualTo(1),
             () -> assertThat(response.photos().get(1).originalName()).isEqualTo("photo2"),
-            () -> assertThat(response.photos().get(2).originalName()).isEqualTo("photo3")
+            () -> assertThat(response.photos().get(1).order()).isEqualTo(2),
+            () -> assertThat(response.photos().get(2).originalName()).isEqualTo("photo3"),
+            () -> assertThat(response.photos().get(2).order()).isEqualTo(3)
         );
     }
 
@@ -152,30 +142,11 @@ public class ProductAcceptanceTest extends AcceptanceTest {
     @Test
     public void throwExceptionWhenProductAlreadyExists() {
         // given
-        RegisterProductRequest request = new RegisterProductRequest(
-            "title",
-            "category",
-            "authorName",
-            "description",
-            List.of(
-                new RegisterProductPhotoRequest("photo1", "path1", 1024),
-                new RegisterProductPhotoRequest("photo2", "path2", 2048),
-                new RegisterProductPhotoRequest("photo3", "path3", 4096)
-            )
-        );
-
-        RestAssuredMockMvc.given()
-            .body(request)
-            .contentType(ContentType.JSON)
-            .accept(ContentType.JSON)
-            .when()
-            .post("/spaces/%s/products".formatted(space.getCode()))
-            .then()
-            .statusCode(201);
+        registerProduct();
 
         // when, then
         RestAssuredMockMvc.given()
-            .body(request)
+            .body(registerRequest)
             .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
             .when()
@@ -183,5 +154,69 @@ public class ProductAcceptanceTest extends AcceptanceTest {
             .then()
             .statusCode(400)
             .body("message", containsString("이미 등록된"));
+    }
+
+    @DisplayName("작품 정보 수정")
+    @Test
+    public void update() {
+        // given
+        ProductResponse registerResponse = registerProduct();
+        Mockito.doNothing().when(awsS3Cloud).deleteContents(Mockito.anyList());
+
+        // when
+        UpdateProductRequest request = new UpdateProductRequest(
+            "foovar1",
+            null,
+            null,
+            "description",
+            List.of(2L),
+            List.of(
+                new RegisterProductPhotoRequest("photo4", "path4", 1024),
+                new RegisterProductPhotoRequest("photo5", "path5", 1024)
+            )
+        );
+
+        ProductResponse result = RestAssuredMockMvc.given()
+            .body(request)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .when()
+            .patch("/spaces/%s/products".formatted(space.getCode()))
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .as(ProductResponse.class);
+
+        // then
+        assertAll(
+            () -> assertThat(result.id()).isNotNull(),
+            () -> assertThat(result.title()).isEqualTo(request.title()),
+            () -> assertThat(result.category()).isEqualTo(registerResponse.category()),
+            () -> assertThat(result.authorName()).isEqualTo(registerResponse.authorName()),
+            () -> assertThat(result.description()).isEqualTo(request.description()),
+            () -> assertThat(result.photos().get(0).originalName()).isEqualTo("photo1"),
+            () -> assertThat(result.photos().get(0).order()).isEqualTo(1),
+            () -> assertThat(result.photos().get(1).originalName()).isEqualTo("photo3"),
+            () -> assertThat(result.photos().get(1).order()).isEqualTo(2),
+            () -> assertThat(result.photos().get(2).originalName()).isEqualTo("photo4"),
+            () -> assertThat(result.photos().get(2).order()).isEqualTo(3),
+            () -> assertThat(result.photos().get(3).originalName()).isEqualTo("photo5"),
+            () -> assertThat(result.photos().get(3).order()).isEqualTo(4)
+        );
+    }
+
+    private ProductResponse registerProduct() {
+        return RestAssuredMockMvc.given()
+            .body(registerRequest)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .when()
+            .post("/spaces/%s/products".formatted(space.getCode()))
+            .then()
+            .statusCode(201)
+            .extract()
+            .body()
+            .as(ProductResponse.class);
     }
 }
