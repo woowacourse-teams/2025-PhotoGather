e991f1d24f3b0933a5c038b107f1131fe663a40a
H0ngJu <hongju4385@gmail.com>
2025-10-05 21:33:17 +0900

    feat: 공통 footer 분리

---
+++ frontend/src/components/@common/footer/Footer.styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Footer = styled.footer`
+  margin-top: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 30px;
+`;
---
+++ frontend/src/components/@common/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import FooterLogo from '../../../@assets/footer-logo.svg?react';
+import * as S from './Footer.styles';
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <FooterLogo />
+    </S.Footer>
+  );
+};
+
+export default Footer;
