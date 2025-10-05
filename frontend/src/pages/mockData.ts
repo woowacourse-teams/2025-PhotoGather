a151f1f7d6aa957229b25f5db755950af1aa5023
H0ngJu <hongju4385@gmail.com>
2025-10-05 21:33:58 +0900

    chore: host workDetail mock Data 추가

--- frontend/src/pages/mockData.ts
+++ frontend/src/pages/mockData.ts
@@ -1,3 +1,7 @@
+import img1 from '../@assets/workDetail_mock_1.png';
+import img2 from '../@assets/workDetail_mock_2.png';
+import img3 from '../@assets/workDetail_mock_3.png';
+
 export const mockData = {
   title: '2025 PKNU VCD',
   thumbnail: 'https://picsum.photos/200/300',
@@ -13,4 +17,13 @@ export const mockDashboardData = {
   introduction: '매일 오후 1시부터 6시까지 상주합니다',
   instagramId: 'forgather_official',
   email: 'forgather@forgather.me',
+};
+
+export const mockWorkDetail = {
+  title: '프로스페로 series 1.',
+  category: 'Chair',
+  designer: 'leesomyeong',
+  description:
+    '485 x 710 x 700\nBioplastic, Walnut\n\n3D 모델링으로 설계한 바이오 플라스틱 구조는 밴딩우드의 미세한 곡률 변화를 통제하고 고정한다. 이는 예측할 수 없는 것을 통제하려는 시도인 동시에 바라는 바를 가시화한 형태다. 이러한 작업과정과 형태는, 불확실한 미래에 대한 본능적 공포에서 탈피하고자한 주술적 행위의 총체가 된다.',
+  images: [img1, img2, img3],
 };
