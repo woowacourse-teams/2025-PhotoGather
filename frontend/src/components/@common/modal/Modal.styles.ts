cc4c89a4a4125d57c1de98420a90a6b685e0c5e2
Minje Sung <minje813@dgu.ac.kr>
2025-10-05 15:38:42 +0900

    fix: 너무 작은 모바일 환경에서 모달이 보이지 않는 현상 수정

--- frontend/src/components/@common/modal/Modal.styles.ts
+++ frontend/src/components/@common/modal/Modal.styles.ts
@@ -13,13 +13,14 @@ export const ModalContent = styled.div<{
   $size: keyof typeof ModalSize;
 }>`
   min-height: 216px;
-  width: ${({ $size }) => ModalSize[$size]}px;
+  width: 100%;
+  max-width: min(${({ $size }) => ModalSize[$size]}px, 90vw);
+  padding: 16px 20px;
+  border-radius: 8px;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 24px 32px;
-  border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.white};
   color: ${({ theme }) => theme.colors.gray06};
 `;
