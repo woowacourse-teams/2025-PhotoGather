661ae81403957cb3b3e6ac98116e6f360a0f29f2
Minje Sung <minje813@dgu.ac.kr>
2025-10-06 03:45:57 +0900

    feat: FunnelBasePage 작성

---
+++ frontend/src/types/funnel.type.ts
@@ -0,0 +1,19 @@
+export interface FunnelBaseElementProps {
+  title: string;
+  description: string;
+  element: React.ReactNode;
+  onNextButtonClick: () => void;
+  nextButtonDisabled?: boolean;
+  buttonText?: string;
+}
+
+export interface FunnelElementProps<T = string> {
+  onNext: (data: T) => void;
+  initialValue?: T;
+}
+
+export interface ImmediateOpenElementInitialValue {
+  date: string;
+  time: string;
+  isImmediateOpen: boolean | null;
+}
