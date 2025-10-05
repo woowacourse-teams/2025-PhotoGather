b3ac33371cb22d8563b3405b08fc53ded6e98de1
hanheel <hanhee630630@gmail.com>
2025-10-05 20:27:09 +0900

    feat: 스페이스 대시보드 수정 페이지 추가

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/layout/global/layout/Layout';
 import HostMainPage from '../pages/host/mainPage/HostMainPage';
+import SpaceEditPage from '../pages/host/spaceEditPage/SpaceEditPage';
 import MainPage from '../pages/MainPage';
 import type { AppRouteObject } from '../types/route.type';
 
@@ -22,6 +23,11 @@ const routes: AppRouteObject[] = [
             handle: {
               headerIcons: ['share', 'settings'],
             },
+          },
+          {
+            // TODO : dashboard 내부로 변경
+            path: 'edit',
+            element: <SpaceEditPage />,
           },
         ],
       },
