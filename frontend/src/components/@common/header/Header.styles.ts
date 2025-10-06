5cb26afb9f80a522ee67d7744431f2dc6753f5d7
H0ngJu <hongju4385@gmail.com>
2025-10-06 20:01:54 +0900

    feat: isDark에 따른 background 설정 추가

--- frontend/src/components/@common/header/Header.styles.ts
+++ frontend/src/components/@common/header/Header.styles.ts
@@ -11,7 +11,7 @@ export const HeaderContainer = styled.header<{ $mode: HeaderMode }>`
   margin: 0 auto;
   padding: 0 ${({ theme }) => theme.layout.padding.leftRight};
   background-color: ${({ theme, $mode }) =>
-    $mode === 'dark' ? theme.colors.gray06 : theme.colors.background};
+    $mode === 'dark' ? theme.colors.black : theme.colors.background};
 `;
 
 export const Logo = styled.div<{ $mode: HeaderMode }>`
@@ -21,7 +21,7 @@ export const Logo = styled.div<{ $mode: HeaderMode }>`
 
   svg {
     path {
-      fill: ${({ theme, $mode }) => ($mode === 'dark' ? theme.colors.white : theme.colors.gray06)};
+      fill: ${({ theme, $mode }) => ($mode === 'dark' ? theme.colors.white : theme.colors.black)};
     }
   }
 `;
@@ -40,7 +40,7 @@ export const IconButton = styled.button<{ $mode: HeaderMode }>`
   border: none;
   padding: 0;
   cursor: pointer;
-  color: ${({ theme, $mode }) => ($mode === 'dark' ? theme.colors.white : theme.colors.gray06)};
+  color: ${({ theme, $mode }) => ($mode === 'dark' ? theme.colors.white : theme.colors.black)};
   transition: opacity 0.2s;
 
   svg {
