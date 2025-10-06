6484d59820a47d57d35798f21010ad64d6a45314
hanheel <hanhee630630@gmail.com>
2025-10-06 10:06:49 +0900

    feat: deleteModal 컴포넌트 제작

---
+++ frontend/src/components/@common/modal/deleteModal/DeleteModal.tsx
@@ -0,0 +1,46 @@
+import { IoWarning } from 'react-icons/io5';
+import { useToast } from '../../../../hooks/@common/useToast';
+import Button from '../../buttons/button/Button';
+import Modal from '../Modal';
+import * as S from './DeleteModal.styles';
+
+interface DeleteModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const DeleteModal = ({ isOpen, onClose }: DeleteModalProps) => {
+  const { showToast } = useToast();
+  const deleteSpace = () => {
+    console.log('삭제 API 연동 + navigate');
+    onClose();
+    showToast({
+      text: '스페이스가 삭제되었습니다.',
+      type: 'info',
+    });
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal.Backdrop />
+      <Modal.Content>
+        <S.DeleteModalContainer>
+          <IoWarning />
+          <S.DeleteModalMessageContainer>
+            <S.DeleteConfirmComment>
+              정말 삭제하시겠습니까?
+            </S.DeleteConfirmComment>
+            <S.DeleteConfirmDescription>
+              삭제 후에는 복구할 수 없습니다.
+            </S.DeleteConfirmDescription>
+          </S.DeleteModalMessageContainer>
+          <S.ButtonContainer>
+            <Button variant="secondary" text="취소" onClick={onClose} />
+            <Button variant="danger" text="삭제" onClick={deleteSpace} />
+          </S.ButtonContainer>
+        </S.DeleteModalContainer>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
