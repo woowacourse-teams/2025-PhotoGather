e2dffef3e5bff397df6c5aabb988c1948a4d2ab7
hanheel <hanhee630630@gmail.com>
2025-10-06 10:07:15 +0900

    feat: delete 모달 추가

--- frontend/src/pages/host/dashBoard/DashBoard.tsx
+++ frontend/src/pages/host/dashBoard/DashBoard.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Button from '../../../components/@common/buttons/button/Button';
+import DeleteModal from '../../../components/@common/modal/deleteModal/DeleteModal';
 import InfoRow from '../../../components/host/infoRow/InfoRow';
 import { mockDashboardData } from '../../mockData';
 import { Thumbnail } from '../Host.common.styles';
 import * as S from './Dashboard.styles';
 
 const Dashboard = () => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
   return (
     <S.Wrapper>
+      <DeleteModal isOpen={isDeleteModalOpen} onClose={closeDeleteModal} />
       <S.Title>스페이스 정보</S.Title>
       <Thumbnail src={mockDashboardData.thumbnail} />
       <S.InfoRowContainer>
@@ -21,9 +31,12 @@ const Dashboard = () => {
       </S.InfoRowContainer>
 
       <S.DeleteButtonContainer>
-        <Button variant="error" text="스페이스 삭제" onClick={() => {}} />
+        <Button
+          variant="error"
+          text="스페이스 삭제"
+          onClick={openDeleteModal}
+        />
       </S.DeleteButtonContainer>
-
       <Button variant="primary" text="수정하기" onClick={() => {}} />
     </S.Wrapper>
   );
