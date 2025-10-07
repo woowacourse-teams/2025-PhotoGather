d1efe7610bcd31b31cd02e171c382c311d25f410
Minje Sung <minje813@dgu.ac.kr>
2025-10-08 03:21:01 +0900

    refactor: SpaceNameElement TextInput 대체

--- frontend/src/pages/create/funnelElements/SpaceNameElement.tsx
+++ frontend/src/pages/create/funnelElements/SpaceNameElement.tsx
@@ -1,17 +1,26 @@
-import TextInput from '../../../components/@common/inputs/TextInput';
+import { useState } from 'react';
+import TextInput from '../../../components/@common/inputs/textInput/TextInput';
 import { CONSTRAINTS } from '../../../constants/constraints';
-import useGraphemeInput from '../../../hooks/@common/useGraphemeInput';
 import type { FunnelElementProps } from '../../../types/funnel.type';
+import { calculateValidLength } from '../../../utils/grapheme';
+import { funnelValidators } from '../funnel/funnel.validators';
 import FunnelBasePage from '../funnel/funnelBasePage/FunnelBasePage';
 
 const SpaceNameElement = ({
   onNext,
   initialValue = '',
 }: FunnelElementProps) => {
-  const { handleChange, validValue, validLength } = useGraphemeInput({
-    initialValue,
-  });
-  const isError = validLength > CONSTRAINTS.NAME_MAX_LENGTH;
+  const [name, setName] = useState(initialValue);
+  const validLength = calculateValidLength(name);
+  let isError = false;
+  let errorMessage = '';
+  try {
+    isError = false;
+    funnelValidators.name(name);
+  } catch (error) {
+    if (error instanceof Error) errorMessage = error.message;
+    isError = true;
+  }
   const isDisabled = isError || validLength === 0;
 
   return (
@@ -20,17 +29,18 @@ const SpaceNameElement = ({
       description="추억을 담을 공간의 이름을 작성해주세요."
       element={
         <TextInput
-          maxCount={CONSTRAINTS.NAME_MAX_LENGTH}
+          isRequired={true}
           validLength={validLength}
-          value={validValue}
-          placeholder="나의 첫 스페이스"
-          onChange={handleChange}
-          errorMessage={
-            isError ? '스페이스 이름은 10자 이하로 지을 수 있어요.' : ''
-          }
+          label="스페이스 이름"
+          placeholder="전시명"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          errorMessage={errorMessage}
+          maxCount={CONSTRAINTS.NAME_MAX_LENGTH}
         />
       }
-      onNextButtonClick={() => onNext(validValue)}
+      onNextButtonClick={() => onNext(name)}
       nextButtonDisabled={isDisabled}
     />
   );
