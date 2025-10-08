16d3a005e350d8c02154f8746e219032c12159ce
hanheel <hanhee630630@gmail.com>
2025-10-08 18:33:41 +0900

    refactor: 변경된 Constraints 객체에 맞춰 형식 변경

--- frontend/src/components/specific/editForm/editForm.validators.ts
+++ frontend/src/components/specific/editForm/editForm.validators.ts
@@ -9,13 +9,13 @@ import {
 export const editFormValidators = {
   name: {
     maxLength: createSafeValidator((value: string) =>
-      checkMaxLength(value, CONSTRAINTS.NAME_MAX_LENGTH),
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.SPACE.NAME),
     ),
     inputEmpty: createSafeValidator((value: string) => checkInputEmpty(value)),
   },
   description: {
     maxLength: createSafeValidator((value: string) =>
-      checkMaxLength(value, CONSTRAINTS.DESCRIPTION_MAX_LENGTH),
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.SPACE.DESCRIPTION),
     ),
     inputEmpty: createSafeValidator((value: string) => checkInputEmpty(value)),
   },
