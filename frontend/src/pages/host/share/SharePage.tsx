b7da0baac23b9a2ec7531871ce2d7310c3522a4b
Minje Sung <minje813@dgu.ac.kr>
2025-10-08 18:08:19 +0900

    feat: SharePage 디자인

--- frontend/src/pages/host/share/SharePage.tsx
+++ frontend/src/pages/host/share/SharePage.tsx
@@ -1,9 +1,86 @@
+import { useRef } from 'react';
+import { MdDownload, MdLink } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import CompleteImage from '../../../@assets/images/space-create.png';
+import Button from '../../../components/@common/buttons/button/Button';
+import IconButton from '../../../components/@common/buttons/iconButton/IconButton';
+import QRCode from '../../../components/@common/qrCode/QRCode';
+import useImageDownload from '../../../hooks/@common/useImageDownload';
+import { useToast } from '../../../hooks/@common/useToast';
+import { copyLinkToClipboard } from '../../../utils/copyLinkToClipboard';
 import * as S from './SharePage.styles';
 
 const SharePage = () => {
+  const navigate = useNavigate();
+
+  // TODO: 로직 구현 후 대체 필요
+  // const location = useLocation();
+  // const { spaceCode } = location.state;
+  const spaceCode = '123456789';
+
+  const { showToast } = useToast();
+  const qrCodeRef = useRef<HTMLCanvasElement>(null);
+  const { saveImage } = useImageDownload();
+  const copyAddress = `${import.meta.env.VITE_DOMAIN}/guest/main`;
+
+  const handleSpaceHomeButton = () => {
+    navigate('../main');
+  };
+
+  const handleMainButton = () => {
+    navigate('/');
+  };
+
+  const copyShareLink = () => {
+    copyLinkToClipboard(copyAddress);
+    showToast({
+      text: '링크가 복사되었습니다.',
+      type: 'info',
+    });
+  };
+
+  const saveQRCodeImage = async () => {
+    const canvas = qrCodeRef.current;
+    if (!canvas) return;
+
+    const blob = await new Promise<Blob | null>((resolve) =>
+      canvas.toBlob(resolve, 'image/png'),
+    );
+    if (!blob) return;
+    saveImage(blob, 'qrcode_share.png');
+  };
+
   return (
     <S.Wrapper>
-      <p>공유 페이지임</p>
+      <S.TopContainer>
+        <S.Image src={CompleteImage} alt="링크 이미지" />
+        <S.TextContainer>
+          <S.Title>스페이스 생성이 완료됐어요</S.Title>
+        </S.TextContainer>
+      </S.TopContainer>
+      {spaceCode ? (
+        <S.BottomContainer>
+          <S.ShareContainer>
+            <QRCode address={copyAddress} ref={qrCodeRef} />
+            <S.ShareLabel>스페이스 링크를 공유해 보세요.</S.ShareLabel>
+            <S.IconLabelButtonContainer>
+              <IconButton
+                icon={<MdDownload />}
+                variant="dark"
+                onClick={saveQRCodeImage}
+              />
+              <IconButton
+                icon={<MdLink style={{ rotate: '-45deg' }} />}
+                variant="dark"
+                onClick={copyShareLink}
+              />
+            </S.IconLabelButtonContainer>
+          </S.ShareContainer>
+          <Button text="나의 스페이스로 이동" onClick={handleSpaceHomeButton} />
+        </S.BottomContainer>
+      ) : (
+        <Button text="메인 페이지로 이동" onClick={handleMainButton} />
+      )}
     </S.Wrapper>
   );
 };
