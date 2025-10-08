7385d497429aa83e9c800deaaf0b25cc386f95c8
hanheel <hanhee630630@gmail.com>
2025-10-08 18:13:43 +0900

    refactor: 함수 간략화

--- frontend/src/utils/grapheme.ts
+++ frontend/src/utils/grapheme.ts
@@ -1,6 +1,5 @@
 const segmenter = new Intl.Segmenter('und', { granularity: 'grapheme' });
 
 export const calculateValidLength = (text: string) => {
-  const graphemes = Array.from(segmenter.segment(text), (s) => s.segment);
-  return graphemes.length;
+  return Array.from(segmenter.segment(text), (s) => s.segment).length;
 };
