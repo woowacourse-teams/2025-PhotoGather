4a388711cd112cd07a2aa9082a089fe074ce2e32
hanheel <hanhee630630@gmail.com>
2025-10-08 18:33:53 +0900

    refactor: react hook form으로 마이그레이션

--- frontend/src/pages/host/workForm/WorkForm.tsx
+++ frontend/src/pages/host/workForm/WorkForm.tsx
@@ -1,14 +1,15 @@
+import { useForm } from 'react-hook-form';
 import { IoClose } from 'react-icons/io5';
 import Button from '../../../components/@common/buttons/button/Button';
 import Textarea from '../../../components/@common/inputs/textArea/Textarea';
 import TextInput from '../../../components/@common/inputs/textInput/TextInput';
 import PhotoUploadButton from '../../../components/host/photoUploadButton/PhotoUploadButton';
-import useForm from '../../../hooks/@common/useForm';
+import { CONSTRAINTS } from '../../../constants/constraints';
 import useLocalFile from '../../../hooks/@common/useLocalFileTmp';
 import { calculateValidLength } from '../../../utils/grapheme';
 import * as S from './WorkForm.styles';
+import { workFormValidators } from './workForm.validators';
 
-//TODO: react-hook-form 도입 이후 삭제 예정
 interface WorkFormData {
   title: string;
   category: string;
@@ -16,97 +17,65 @@ interface WorkFormData {
   description: string;
 }
 
-const MAX_LENGTH = {
-  title: 50,
-  category: 20,
-  designer: 20,
-  description: 1000,
-} as const;
-
 const WorkForm = () => {
-  const { formData, handleChange } = useForm<WorkFormData>({
-    initialData: {
-      title: '',
-      category: '',
-      designer: '',
-      description: '',
-    },
-    validators: {
-      title: (value: string) => {
-        if (calculateValidLength(value) > MAX_LENGTH.title) {
-          throw new Error(`작품명은 ${MAX_LENGTH.title}자 이내로 입력해주세요`);
-        }
-      },
-      category: (value: string) => {
-        if (calculateValidLength(value) > MAX_LENGTH.category) {
-          throw new Error(
-            `카테고리는 ${MAX_LENGTH.category}자 이내로 입력해주세요`,
-          );
-        }
-      },
-      designer: (value: string) => {
-        if (calculateValidLength(value) > MAX_LENGTH.designer) {
-          throw new Error(
-            `작가명은 ${MAX_LENGTH.designer}자 이내로 입력해주세요`,
-          );
-        }
-      },
-      description: (value: string) => {
-        if (calculateValidLength(value) > MAX_LENGTH.description) {
-          throw new Error(
-            `작품 설명은 ${MAX_LENGTH.description}자 이내로 입력해주세요`,
-          );
-        }
-      },
-    },
-    onSubmit: () => {
-      // TODO: 작품 소개 등록 로직
-    },
+  const initialData: WorkFormData = {
+    title: '',
+    category: '',
+    designer: '',
+    description: '',
+  };
+
+  const {
+    register,
+    // handleSubmit,
+    watch,
+    formState: { errors, isValid: isAllValid },
+  } = useForm<WorkFormData>({
+    mode: 'onChange',
+    defaultValues: initialData,
   });
 
   const { previewFile, deleteFile, handleFilesUploadClick, handleFilesDrop } =
     useLocalFile({ fileType: 'image' });
 
-  const isFormValid =
-    formData.title.trim() !== '' && formData.description.trim() !== '';
-
   return (
     <S.Wrapper>
       <S.TitleContainer>작품 소개 등록</S.TitleContainer>
-      <S.FormContainer>
+      {/* <S.FormContainer onSubmit={handleSubmit(onValid)}> */}
+      <S.FormContainer onSubmit={() => {}}>
         <S.FormLabelContainer>
           <TextInput
+            {...register('title', {
+              validate: workFormValidators.title,
+            })}
             label="작품명"
             isRequired
-            name="title"
-            maxCount={MAX_LENGTH.title}
-            value={formData.title}
-            validLength={calculateValidLength(formData.title)}
-            onChange={handleChange}
+            validLength={calculateValidLength(watch('title'))}
+            maxCount={CONSTRAINTS.MAX_LENGTH.WORK.TITLE}
             placeholder="작품명을 입력하세요"
+            errorMessage={errors.title?.message}
           />
         </S.FormLabelContainer>
 
         <S.FormLabelContainer>
           <TextInput
+            {...register('category', {
+              validate: workFormValidators.category,
+            })}
             label="카테고리"
-            name="category"
-            maxCount={MAX_LENGTH.category}
-            value={formData.category}
-            validLength={calculateValidLength(formData.category)}
-            onChange={handleChange}
+            maxCount={CONSTRAINTS.MAX_LENGTH.WORK.CATEGORY}
             placeholder="카테고리를 입력하세요"
+            validLength={calculateValidLength(watch('category'))}
           />
         </S.FormLabelContainer>
 
         <S.FormLabelContainer>
           <TextInput
+            {...register('designer', {
+              validate: workFormValidators.designer,
+            })}
             label="작가명"
-            name="designer"
-            maxCount={MAX_LENGTH.designer}
-            value={formData.designer}
-            validLength={calculateValidLength(formData.designer)}
-            onChange={handleChange}
+            maxCount={CONSTRAINTS.MAX_LENGTH.WORK.DESIGNER}
             placeholder="작가명을 입력하세요"
           />
         </S.FormLabelContainer>
@@ -114,13 +83,13 @@ const WorkForm = () => {
         <S.FormLabelContainer>
           <S.LabelContainer>작품 설명 *</S.LabelContainer>
           <Textarea
-            name="description"
-            maxCount={MAX_LENGTH.description}
-            value={formData.description}
-            validLength={calculateValidLength(formData.description)}
-            onChange={handleChange}
+            {...register('description', {
+              validate: workFormValidators.description,
+            })}
+            maxCount={CONSTRAINTS.MAX_LENGTH.WORK.DESCRIPTION}
             placeholder="작품 설명을 입력하세요"
             rows={6}
+            validLength={calculateValidLength(watch('description'))}
           />
         </S.FormLabelContainer>
 
@@ -163,10 +132,10 @@ const WorkForm = () => {
       </S.FormContainer>
       <S.ButtonContainer>
         <Button
+          type="submit"
           text="작품 소개 등록하기"
-          onClick={() => {}}
           variant="tertiary"
-          disabled={!isFormValid}
+          disabled={!isAllValid}
         />
       </S.ButtonContainer>
     </S.Wrapper>
