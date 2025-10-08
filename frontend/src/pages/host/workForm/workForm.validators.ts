03b787ccef3eac6bc420f451b37fa1ce80ff7192
hanheel <hanhee630630@gmail.com>
2025-10-08 18:33:17 +0900

    feat: workForm의 validation 객체를 정의

---
+++ frontend/src/pages/host/workForm/workForm.validators.ts
@@ -0,0 +1,31 @@
+import { CONSTRAINTS } from '../../../constants/constraints';
+import { createSafeValidator } from '../../../utils/validateForm';
+import {
+  checkInputEmpty,
+  checkMaxLength,
+} from '../../../validators/form.validators';
+
+export const workFormValidators = {
+  title: {
+    maxLength: createSafeValidator((value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.WORK.TITLE),
+    ),
+    inputEmpty: createSafeValidator((value: string) => checkInputEmpty(value)),
+  },
+  category: {
+    maxLength: createSafeValidator((value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.WORK.CATEGORY),
+    ),
+  },
+  designer: {
+    maxLength: createSafeValidator((value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.WORK.DESIGNER),
+    ),
+  },
+  description: {
+    maxLength: createSafeValidator((value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.WORK.DESCRIPTION),
+    ),
+    inputEmpty: createSafeValidator((value: string) => checkInputEmpty(value)),
+  },
+};
