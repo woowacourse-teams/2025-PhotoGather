0e559bf5235ddde090cb327999c40c2f071e29c0
Minje Sung <minje813@dgu.ac.kr>
2025-10-08 17:24:13 +0900

    refactor: styles formatter 적용

--- frontend/src/pages/host/spaceCreate/funnelElements/spaceDetailElement/SpaceDetailElement.styles.tsx
+++ frontend/src/pages/host/spaceCreate/funnelElements/spaceDetailElement/SpaceDetailElement.styles.tsx
@@ -1,35 +1,27 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    gap: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 32px;
 `;
 
 export const ImageUploadContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    align-items: center;
-`;
-
-export const ImagePreviewBox = styled.div`
-    width: 120px;
-    height: 120px;
-    border-radius: 4px;
-    background-color: #E0E0E0;
-    cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
 `;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 
 export const Label = styled.label`
-    font-size: 14px;
-    font-weight: 500;
-    color: #333333;
+  font-size: 14px;
+  font-weight: 500;
+  color: #333333;
 `;
