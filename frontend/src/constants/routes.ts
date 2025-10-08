75e5e9cfc1fbfb4816594c77862847a571505243
hanheel <hanhee630630@gmail.com>
2025-10-08 22:44:01 +0900

    refactor: route 상수 분리

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,14 @@
+export const ROUTES = {
+  HOST: {
+    MAIN: '/host/main',
+    DASHBOARD: '/host/dashboard',
+    DASHBOARD_EDIT: '/host/dashboard-edit',
+    MY_PAGE: '/host/my-page',
+    WORK_DETAIL: '/host/work-detail',
+    WORK_FORM: '/host/work-form',
+  },
+  GUEST: {
+    MAIN: '/guest/main',
+    WORK_DETAIL: '/guest/work-detail',
+  },
+};
