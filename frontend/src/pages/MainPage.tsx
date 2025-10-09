62420f07b7a99446f251262d1c3a8575bdb5ea05
Minje Sung <minje813@dgu.ac.kr>
2025-10-09 18:57:36 +0900

    feat: 데모페이지 요소 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,31 @@
-import { MdInsertEmoticon } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/@common/buttons/button/Button';
+import { ROUTES } from '../constants/routes';
 
 const MainPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <MdInsertEmoticon />
-      <p>글꼴 확인</p>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '16px',
+        alignItems: 'center',
+      }}
+    >
+      <h1>포게더 2.0 데모페이지</h1>
+      <Button
+        text="[Host] Mypage 이동 (로그인)"
+        onClick={() => navigate(ROUTES.HOST.MY_PAGE)}
+      />
+      <Button
+        text="[GUEST] 스페이스 메인 이동"
+        onClick={() => navigate(ROUTES.GUEST.MAIN)}
+      />
+      <Button
+        text="[GUEST] 작품 소개 이동"
+        onClick={() => navigate(ROUTES.GUEST.WORK_DETAIL)}
+      />
     </div>
   );
 };
