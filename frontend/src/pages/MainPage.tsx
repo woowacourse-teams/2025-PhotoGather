9ccb604408a27a5f9ddc30a50dcf88e7327d29d7
Minje Sung <minje813@dgu.ac.kr>
2025-10-09 18:50:26 +0900

    fix: 글꼴이 미적용되고 있던 현상 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,19 +1,10 @@
 import { MdInsertEmoticon } from 'react-icons/md';
-import { useToast } from '../hooks/@common/useToast';
 
 const MainPage = () => {
-  const { showToast } = useToast();
-
   return (
     <div>
       <MdInsertEmoticon />
       <p>글꼴 확인</p>
-      <button
-        type="button"
-        onClick={() => showToast({ text: '토스트 테스트' })}
-      >
-        토스트 테스트
-      </button>
     </div>
   );
 };
