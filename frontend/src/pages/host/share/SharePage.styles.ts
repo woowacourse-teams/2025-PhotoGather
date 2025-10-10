26573bceb34091881cae284497ad4e3f9449c44e
H0ngJu <hongju4385@gmail.com>
2025-10-10 19:57:29 +0900

    style: format:styles 적용

--- frontend/src/pages/host/share/SharePage.styles.ts
+++ frontend/src/pages/host/share/SharePage.styles.ts
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  height: ${({ theme }) => `calc(100dvh - ${parseInt(theme.layout.padding.topBottom, 10) * 2}px - ${parseInt(theme.layout.headerHeight, 10)}px)`};
+  height: ${({ theme }) =>
+    `calc(100dvh - ${parseInt(theme.layout.padding.topBottom, 10) * 2}px - ${parseInt(theme.layout.headerHeight, 10)}px)`};
   display: flex;
   flex-direction: column;
   justify-content: space-between;
--- frontend/src/pages/host/workForm/WorkForm.styles.ts
+++ frontend/src/pages/host/workForm/WorkForm.styles.ts
@@ -105,4 +105,4 @@ export const DeleteButton = styled.button`
 export const TopContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  `;
+`;
