211f8f674d735c6695aec1ea0daeda2d4374e4b5
Minje Sung <minje813@dgu.ac.kr>
2025-10-14 20:58:00 +0900

    refactor: auto 개수 축소

--- frontend/src/components/@common/footer/Footer.styles.ts
+++ frontend/src/components/@common/footer/Footer.styles.ts
@@ -3,7 +3,7 @@ import type { FooterMode } from '../../../types/uiMode.type';
 
 export const Footer = styled.footer<{ $mode: FooterMode }>`
   max-width: ${({ theme }) => theme.layout.width};
-  margin: auto auto 0 auto;
+  margin: auto auto 0;
   display: flex;
   justify-content: center;
   align-items: center;
