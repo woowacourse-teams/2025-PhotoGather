e73864454f10724bd97cd9b65fd7dd026628e65d
Minje Sung <minje813@dgu.ac.kr>
2025-10-14 20:42:24 +0900

    feat: 작성한 mock 데이터로 적용

--- frontend/src/pages/host/guestbook/list/GuestbookListPage.tsx
+++ frontend/src/pages/host/guestbook/list/GuestbookListPage.tsx
@@ -1,18 +1,27 @@
+import { mockGuestbookCards } from '../../../mockData';
 import GuestbookElement from './element/GuestbookElement';
 import * as S from './GuestbookListPage.styles';
 
 const GuestbookListPage = () => {
+  const guestbookCardsResponse = mockGuestbookCards;
+
   return (
     <S.Wrapper>
       <S.InfoContainer>
         <S.Title>밍고의 전시회 방명록</S.Title>
-        <S.Description>28명 참여</S.Description>
+        <S.Description>
+          {guestbookCardsResponse.guestBookCards.length}명 참여
+        </S.Description>
       </S.InfoContainer>
       <S.ListContainer>
-        <GuestbookElement guestName={'밍고'} hasPhoto={true} isNew={true} />
-        <GuestbookElement guestName={'블루블루'} hasPhoto={true} isNew={true} />
-        <GuestbookElement guestName={'퐁퐁쥬'} hasPhoto={true} isNew={true} />
-        <GuestbookElement guestName={'루나'} hasPhoto={true} isNew={true} />
+        {guestbookCardsResponse.guestBookCards.map((card) => (
+          <GuestbookElement
+            key={card.id}
+            guestName={card.nickname}
+            hasPhoto={card.containsPhoto}
+            isRead={card.isRead}
+          />
+        ))}
       </S.ListContainer>
     </S.Wrapper>
   );
