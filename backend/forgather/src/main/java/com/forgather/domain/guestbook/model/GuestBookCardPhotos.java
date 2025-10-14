1b4476e53c14ecc73951e1a46228cb0c5f448f9c
kjyyjk <rlawnsdud920@gmail.com>
2025-10-14 15:30:32 +0900

    feat: GuestBookCardPhotos#deleteByIds 구현 및 테스트

--- backend/forgather/src/main/java/com/forgather/domain/guestbook/model/GuestBookCardPhotos.java
+++ backend/forgather/src/main/java/com/forgather/domain/guestbook/model/GuestBookCardPhotos.java
@@ -2,8 +2,11 @@ package com.forgather.domain.guestbook.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.forgather.domain.model.Photo;
 import com.forgather.global.exception.BaseException;
 
 public class GuestBookCardPhotos {
@@ -25,5 +28,30 @@ public class GuestBookCardPhotos {
 
     public List<GuestBookCardPhoto> getAll() {
         return Collections.unmodifiableList(photos);
+    }
+
+    public List<GuestBookCardPhoto> deleteByIds(List<Long> deleteIds) {
+        validateIds(deleteIds);
+        List<GuestBookCardPhoto> deletedPhotos = getDeletedPhotos(deleteIds);
+        photos.removeAll(deletedPhotos);
+        return deletedPhotos;
+    }
+
+    private void validateIds(List<Long> deleteIds) {
+        Set<Long> photosIdSet = new HashSet<>(photos.stream().map(Photo::getId).toList());
+        for (Long deleteId : deleteIds) {
+            if (photosIdSet.contains(deleteId)) {
+                continue;
+            }
+            throw new BaseException("방명록 카드에 존재하지 않는 사진입니다. deletePhotoId: " + deleteId);
+        }
+    }
+
+    private List<GuestBookCardPhoto> getDeletedPhotos(List<Long> deleteIds) {
+        Set<Long> deleteIdSet = new HashSet<>(deleteIds);
+        List<GuestBookCardPhoto> deletedPhotos = photos.stream()
+            .filter(photo -> deleteIdSet.contains(photo.getId()))
+            .toList();
+        return deletedPhotos;
     }
 }
