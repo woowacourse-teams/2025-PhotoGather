9505f59783dbd91477050b1ea87cff9b67d31993
hanheel <hanhee630630@gmail.com>
2025-10-14 16:13:36 +0900

    feat: 방명록 입력값에 대한 validator 추가

---
+++ frontend/src/pages/guest/guestbookPage/funnel/funnel.validators.ts
@@ -0,0 +1,16 @@
+import { CONSTRAINTS } from '../../../../constants/constraints';
+import {
+  checkInputEmpty,
+  checkMaxLength,
+} from '../../../../validators/form.validators';
+
+export const funnelValidators = {
+  message: {
+    maxLength: (value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.GUESTBOOK.MESSAGE),
+  },
+  nickname: {
+    maxLength: (value: string) =>
+      checkMaxLength(value, CONSTRAINTS.MAX_LENGTH.GUESTBOOK.NICKNAME),
+  },
+};
