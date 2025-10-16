a13d0d6377b39e9889cf89a73756340b705d7e35
H0ngJu <hongju4385@gmail.com>
2025-10-16 16:28:24 +0900

    feat: 사진 상세 모달 구현

---
+++ frontend/src/components/specific/modal/photoModal/PhotoModal.style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+width: 100%;
+display: flex;
+    flex-direction: column;
+    gap: 8px;
+    align-items: center;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+`;
