839e794fc3b6e4cbe2eed381a019556f895c5569
kjyyjk <rlawnsdud920@gmail.com>
2025-10-16 15:35:40 +0900

    refactor: oom 방지용 queue 상한선 설정

--- backend/forgather/src/main/java/com/forgather/global/config/AsyncConfig.java
+++ backend/forgather/src/main/java/com/forgather/global/config/AsyncConfig.java
@@ -12,13 +12,7 @@ import com.forgather.global.logging.LoggingTaskDecorator;
 @EnableAsync
 public class AsyncConfig {
     /**
-     * maxPoolSize queueCapacity는 기본 설정 사용한다.
-     * 기본 설정 둘 다 Integer.MAX_VALUE 이다.
-     * ThreadPoolTaskExecutor는 유휴 스레드가 존재하지 않고 대기큐가 꽉차면 max 아래로 스레드를 추가 생성한다.
-     * 이론 상 대기큐를 꽉 채우지 못하므로 maxPoolSize는 의미 없는 설정이라고 볼 수 있다.
-     * 추가 생성 없이 지정한 coreThread 수만큼의 스레드가 작업을 처리한다.
-     *
-     * 해당 스레드풀에 대한 모니터링 추가 후 감소/증가해볼 수 있을 것이다.
+     * 해당 스레드풀에 대한 모니터링 추가 후 설정 값들을 조정해볼 수 있다.
      */
     @Bean
     public TaskExecutor taskExecutor() {
@@ -31,6 +25,7 @@ public class AsyncConfig {
          * 우선 2 * 2 = 4로 설정함.
          */
         taskExecutor.setCorePoolSize(4);
+        taskExecutor.setQueueCapacity(1000); // OOM 방지용
 
         taskExecutor.setTaskDecorator(new LoggingTaskDecorator());
         taskExecutor.setThreadNamePrefix("async-task-");
