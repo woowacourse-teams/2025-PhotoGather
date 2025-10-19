74b19bc28164a6a2d492e39fd44b86354d1ea690
kjyyjk <rlawnsdud920@gmail.com>
2025-10-19 17:35:41 +0900

    refactor: 작품 관련 요청 처리 시 스페이스 존재 검증

--- backend/forgather/src/main/java/com/forgather/domain/product/repository/ProductRepository.java
+++ backend/forgather/src/main/java/com/forgather/domain/product/repository/ProductRepository.java
@@ -5,21 +5,22 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 
 import com.forgather.domain.product.model.Product;
+import com.forgather.domain.space.model.Space;
 import com.forgather.global.exception.BaseNullPointerException;
 import com.forgather.global.exception.NotFoundException;
 
 public interface ProductRepository {
     Product save(Product product);
 
-    Optional<Product> findBySpaceCode(String spaceCode);
-
-    default Product getBySpaceCodeOrThrow(String spaceCode) {
-        if (spaceCode == null) {
-            throw new BaseNullPointerException("스페이스의 코드는 null일 수 없습니다.", HttpStatus.BAD_REQUEST);
-        }
-        return findBySpaceCode(spaceCode)
-            .orElseThrow(() -> new NotFoundException("해당 스페이스에 등록된 작품이 없습니다. spaceCode: " + spaceCode));
-    }
-
     void delete(Product product);
+
+    Optional<Product> findBySpace(Space space);
+
+    default Product getBySpaceOrThrow(Space space) {
+        if (space == null) {
+            throw new BaseNullPointerException("스페이스는 null일 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return findBySpace(space)
+            .orElseThrow(() -> new NotFoundException("해당 스페이스에 등록된 작품이 없습니다. spaceCode: " + space.getCode()));
+    }
 }
