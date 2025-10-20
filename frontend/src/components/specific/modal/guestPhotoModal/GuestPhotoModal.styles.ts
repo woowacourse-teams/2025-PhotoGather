9628512c53522864bd4a89798eec9dafd5230237
Minje Sung <minje813@dgu.ac.kr>
2025-10-20 16:03:45 +0900

    fix: guest 방명록 모달 zindex 조정

--- frontend/src/components/specific/modal/guestPhotoModal/GuestPhotoModal.styles.ts
+++ frontend/src/components/specific/modal/guestPhotoModal/GuestPhotoModal.styles.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
 width: 100%;
+z-index: ${({ theme }) => theme.zIndex.modal};
 display: flex;
     flex-direction: column;
     gap: 8px;
