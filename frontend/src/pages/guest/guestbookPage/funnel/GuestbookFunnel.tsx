4ba4183821c227301e403fc7643a0492b59d431b
hanheel <hanhee630630@gmail.com>
2025-10-21 21:51:25 +0900

    feat: 이미지가 없을 경우 Fallback 이미지 대체

--- frontend/src/pages/guest/guestbookPage/funnel/GuestbookFunnel.tsx
+++ frontend/src/pages/guest/guestbookPage/funnel/GuestbookFunnel.tsx
@@ -38,7 +38,7 @@ const GuestBookFunnel = () => {
   const receiver = spaceInfo.name || '방명록 주인장';
   const thumbnailUrl = spaceInfo.spacePhoto.isExists
     ? buildOriginalImageUrl(spaceInfo.spacePhoto.path)
-    : '';
+    : 'invalid-url';
 
   const { submitForm, isLoading } = usePostGuestbook({
     spaceCode: spaceCode ?? '',
@@ -59,7 +59,7 @@ const GuestBookFunnel = () => {
       <LoadingModal isOpen={isLoading} text="전송 중..." />
       <S.Wrapper>
         <S.DisplayInfoContainer>
-          <DisplayProfile src={thumbnailUrl} alt="전시 썸네일 이미지" />
+          <DisplayProfile src={thumbnailUrl} alt={spaceInfo.name} />
           <S.DisplayName>{spaceInfo.name}</S.DisplayName>
         </S.DisplayInfoContainer>
         <DividerLine width="15%" />
