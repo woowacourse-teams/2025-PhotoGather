f9936bee1fcf05770d9467011510c98c8cb297ec
H0ngJu <hongju4385@gmail.com>
2025-10-21 19:47:14 +0900

    feat: sentry 초기 세팅

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,15 @@
+import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
 export const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.VITE_ENVIRONMENT,
+  sendDefaultPii: true,
+});
 
 // biome-ignore lint/style/noNonNullAssertion : 루트에서 non-null 무시
 createRoot(document.getElementById('root')!).render(
