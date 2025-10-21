324d50c7497e60e30f024bb7a1d9a7f631c338fe
hanheel <hanhee630630@gmail.com>
2025-10-21 19:16:36 +0900

    feat: HostMain 페이지에 공유 모달 연결

--- frontend/src/pages/host/mainPage/HostMainPage.tsx
+++ frontend/src/pages/host/mainPage/HostMainPage.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { IoLogoInstagram, IoShareOutline } from 'react-icons/io5';
-import { MdEdit, MdEmail, MdShare } from 'react-icons/md';
+import { MdEdit, MdEmail } from 'react-icons/md';
 import { useNavigate, useParams } from 'react-router-dom';
 import Button from '../../../components/@common/buttons/button/Button';
 import IconButton from '../../../components/@common/buttons/iconButton/IconButton';
 import Thumbnail from '../../../components/@common/thumbnail/Thumbnail';
+import SpaceShareModal from '../../../components/specific/modal/spaceShareModal/SpaceShareModal';
 import {
   createGuestbookRoute,
   createSpaceInfoEditRoute,
@@ -23,77 +25,88 @@ const HostMainPage = () => {
     spaceCode,
   });
 
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
+  const openShareModal = () => {
+    setIsShareModalOpen(true);
+  };
+  const closeShareModal = () => {
+    setIsShareModalOpen(false);
+  };
+
   return (
-    <MainPageStyles.Wrapper>
-      <S.ActionButtonContainer>
-        <IconButton
-          aria-label="스페이스 정보 수정"
-          icon={<MdEdit size={12} />}
-          variant="default"
-          size="small"
-          onClick={() => {
-            navigate(createSpaceInfoEditRoute(spaceCode));
-          }}
-        />
-        <IconButton
-          aria-label="스페이스 공유"
-          icon={<IoShareOutline size={12} />}
-          size="small"
-          variant="default"
-          onClick={() => {}}
-        />
-      </S.ActionButtonContainer>
-      <MainPageStyles.ProfileContainer>
-        <Thumbnail src={buildOriginalImageUrl(spaceInfo.spacePhoto.path)} />
-        <MainPageStyles.InfoContainer>
-          <MainPageStyles.Name>{spaceInfo.name}</MainPageStyles.Name>
-          <MainPageStyles.Introduction>
-            {spaceInfo.description}
-          </MainPageStyles.Introduction>
-        </MainPageStyles.InfoContainer>
-      </MainPageStyles.ProfileContainer>
-      <MainPageStyles.IconButtonContainer>
-        <IconButton
-          aria-label="인스타그램"
-          icon={<IoLogoInstagram size={24} />}
-          variant="default"
-          onClick={() =>
-            window.open(
-              createInstagramUrl(spaceInfo.instagramUsername),
-              '_blank',
-            )
-          }
-          disabled={
-            !spaceInfo.instagramUsername || spaceInfo.instagramUsername === ''
-          }
-        />
-        <IconButton
-          aria-label="이메일"
-          icon={<MdEmail size={24} />}
-          variant="default"
-          onClick={() => window.open(`mailto:${spaceInfo.email}`, '_blank')}
-          disabled={!spaceInfo.email || spaceInfo.email === ''}
-        />
-      </MainPageStyles.IconButtonContainer>
-      <DividerLine width="10%" />
-      <MainPageStyles.ButtonContainer>
-        <Button
-          variant="elevated"
-          text="작품 소개 관리"
-          onClick={() => {
-            navigate(createWorkDetailRoute(spaceCode));
-          }}
-        />
-        <Button
-          variant="elevated"
-          text="방명록 관리"
-          onClick={() => {
-            navigate(createGuestbookRoute(spaceCode));
-          }}
-        />
-      </MainPageStyles.ButtonContainer>
-      <MainPageStyles.Footer />
-    </MainPageStyles.Wrapper>
+    <>
+      <SpaceShareModal isOpen={isShareModalOpen} onClose={closeShareModal} />
+      <MainPageStyles.Wrapper>
+        <S.ActionButtonContainer>
+          <IconButton
+            aria-label="스페이스 정보 수정"
+            icon={<MdEdit size={12} />}
+            variant="default"
+            size="small"
+            onClick={() => {
+              navigate(createSpaceInfoEditRoute(spaceCode));
+            }}
+          />
+          <IconButton
+            aria-label="스페이스 공유"
+            icon={<IoShareOutline size={12} />}
+            size="small"
+            variant="default"
+            onClick={openShareModal}
+          />
+        </S.ActionButtonContainer>
+        <MainPageStyles.ProfileContainer>
+          <Thumbnail src={buildOriginalImageUrl(spaceInfo.spacePhoto.path)} />
+          <MainPageStyles.InfoContainer>
+            <MainPageStyles.Name>{spaceInfo.name}</MainPageStyles.Name>
+            <MainPageStyles.Introduction>
+              {spaceInfo.description}
+            </MainPageStyles.Introduction>
+          </MainPageStyles.InfoContainer>
+        </MainPageStyles.ProfileContainer>
+        <MainPageStyles.IconButtonContainer>
+          <IconButton
+            aria-label="인스타그램"
+            icon={<IoLogoInstagram size={24} />}
+            variant="default"
+            onClick={() =>
+              window.open(
+                createInstagramUrl(spaceInfo.instagramUsername),
+                '_blank',
+              )
+            }
+            disabled={
+              !spaceInfo.instagramUsername || spaceInfo.instagramUsername === ''
+            }
+          />
+          <IconButton
+            aria-label="이메일"
+            icon={<MdEmail size={24} />}
+            variant="default"
+            onClick={() => window.open(`mailto:${spaceInfo.email}`, '_blank')}
+            disabled={!spaceInfo.email || spaceInfo.email === ''}
+          />
+        </MainPageStyles.IconButtonContainer>
+        <DividerLine width="10%" />
+        <MainPageStyles.ButtonContainer>
+          <Button
+            variant="elevated"
+            text="작품 소개 관리"
+            onClick={() => {
+              navigate(createWorkDetailRoute(spaceCode));
+            }}
+          />
+          <Button
+            variant="elevated"
+            text="방명록 관리"
+            onClick={() => {
+              navigate(createGuestbookRoute(spaceCode));
+            }}
+          />
+        </MainPageStyles.ButtonContainer>
+        <MainPageStyles.Footer />
+      </MainPageStyles.Wrapper>
+    </>
   );
 };
 
