4d14b4cb518d5752951767c9e082f3af254ec493
H0ngJu <hongju4385@gmail.com>
2025-10-21 21:55:11 +0900

    feat: Clarity 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import Clarity from '@microsoft/clarity';
 import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
@@ -5,11 +6,14 @@ import App from './App.tsx';
 
 export const queryClient = new QueryClient();
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: import.meta.env.VITE_ENVIRONMENT,
-  sendDefaultPii: true,
-});
+if (import.meta.env.VITE_ENVIRONMENT === 'production') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.VITE_ENVIRONMENT,
+    sendDefaultPii: true,
+  });
+  Clarity.init(import.meta.env.VITE_CLARITY_PROJECT_ID);
+}
 
 // biome-ignore lint/style/noNonNullAssertion : 루트에서 non-null 무시
 createRoot(document.getElementById('root')!).render(
