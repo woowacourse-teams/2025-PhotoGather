db86126a8f4ffc81f5f81180a52c3466fa7e69d4
hanheel <hanhee630630@gmail.com>
2025-10-21 15:29:51 +0900

    design: photoInput이 깜빡이지 않도록 명시적인 width 지정

--- frontend/src/styles/@common/PhotoInput.styles.ts
+++ frontend/src/styles/@common/PhotoInput.styles.ts
@@ -5,12 +5,20 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
+  max-width: 60px;
+  max-height: 60px;
+  width: 100%;
+  height: 100%;
 `;
 
 export const Label = styled.label`
   aspect-ratio: 1/1;
   cursor: pointer;
   position: relative;
+  max-width: 60px;
+  max-height: 60px;
+  width: 100%;
+  height: 100%;
 `;
 
 export const FileInput = styled.input`
