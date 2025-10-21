75f3fb53c2231534e8550354e7181f8595721170
hanheel <hanhee630630@gmail.com>
2025-10-21 21:50:36 +0900

    feat: 햄버거 컴포넌트 구현

---
+++ frontend/src/components/@common/hamburger/Hamburger.styles.ts
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+import { Backdrop } from '../../../styles/@common/Backdrop.styles';
+
+export const Wrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  width: ${({ theme }) => theme.layout.width};
+  height: 100dvh;
+  z-index: ${({ theme }) => theme.zIndex.modalBackdrop};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const HamburgerBackdrop = styled(Backdrop)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+`;
+
+export const HamburgerBackground = styled.div<{ $isOpen: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.white};
+  z-index: ${({ theme }) => theme.zIndex.modal};
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  opacity: ${({ $isOpen }) => ($isOpen ? 1 : 0)};
+  transform: ${({ $isOpen }) => ($isOpen ? 'translateX(0)' : 'translateX(100%)')};
+  margin-left: auto;
+`;
+export const ItemContainer = styled.ul`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray02};
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`;
+
+export const Item = styled.li`
+  ${({ theme }) => ({
+    ...theme.typography.bodyLarge,
+  })}
+  color: ${({ theme }) => theme.colors.gray06};
+`;
---
+++ frontend/src/components/@common/hamburger/Hamburger.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import type { NavigateInfo } from '../../../types/route.type';
+import * as S from './Hamburger.styles';
+
+interface HamburgerProps {
+  navigateInfo: NavigateInfo[];
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+const Hamburger = ({ isOpen, onClose, navigateInfo }: HamburgerProps) => {
+  return (
+    <>
+      {isOpen && (
+        <S.Wrapper>
+          <S.HamburgerBackdrop onClick={onClose} />
+
+          <S.HamburgerBackground $isOpen={isOpen}>
+            {navigateInfo.map((info) => (
+              <S.ItemContainer key={info.name}>
+                <S.Item>
+                  <Link to={info.path} onClick={onClose}>
+                    {info.name}
+                  </Link>
+                </S.Item>
+              </S.ItemContainer>
+            ))}
+          </S.HamburgerBackground>
+        </S.Wrapper>
+      )}
+    </>
+  );
+};
+
+export default Hamburger;
