337e909a032ea4ad0d573ea4d39e939b23d23c07
hanheel <hanhee630630@gmail.com>
2025-10-22 14:17:23 +0900

    refactor: 불필요한 import 삭제

--- frontend/src/hooks/domain/space/useSpaceDelete.ts
+++ frontend/src/hooks/domain/space/useSpaceDelete.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { spaceService } from '../../../apis/services/space/space.service';
-import { createSpaceMainRoute, ROUTES } from '../../../constants/routes';
+import { ROUTES } from '../../../constants/routes';
 import { useToast } from '../../@common/useToast';
 
 interface UseSpaceDeleteProps {
