58c32f67a24f419abafa6444f5e58a65b5e265e0
Minje Sung <minje813@dgu.ac.kr>
2025-10-22 11:07:25 +0900

    style: formatter 적용

--- frontend/src/pages/landing/LandingPage.styles.ts
+++ frontend/src/pages/landing/LandingPage.styles.ts
@@ -13,107 +13,108 @@ export const Wrapper = styled.div`
 `;
 
 export const ContentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 300px;
-    padding: 120px 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 300px;
+  padding: 120px 0px;
 `;
 
 export const Section = styled.section`
-    display: flex;
-    flex-direction: column;
-    gap: 36px;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+  align-items: center;
 `;
 
 export const Title = styled.h1`
-    ${({ theme }) => theme.typography.header02}
-    color: ${({ theme }) => theme.colors.white};
-    white-space: pre-wrap;
+  ${({ theme }) => theme.typography.header02}
+  color: ${({ theme }) => theme.colors.white};
+  white-space: pre-wrap;
 `;
 
 export const SmallTitle = styled.p`
-    ${({ theme }) => theme.typography.header03}
-    color: ${({ theme }) => theme.colors.white};
-    white-space: pre-wrap;
-    margin: 64px 0px;
+  ${({ theme }) => theme.typography.header03}
+  color: ${({ theme }) => theme.colors.white};
+  white-space: pre-wrap;
+  margin: 64px 0px;
 `;
 
 export const SubTitle = styled.h2`
-    ${({ theme }) => theme.typography.header02}
-    color: ${({ theme }) => theme.colors.white};
-    text-align: center;
-    line-height: 36px;
-    white-space: pre-wrap;
+  ${({ theme }) => theme.typography.header02}
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
+  line-height: 36px;
+  white-space: pre-wrap;
 `;
 
 export const DescriptionBox = styled.div`
-    ${({ theme }) => theme.typography.bodyRegular}
+  ${({ theme }) => theme.typography.bodyRegular}
 `;
 
 export const DescriptionTitle = styled.p`
-    ${({ theme }) => theme.typography.header03}
-    color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => hexToRgba(theme.colors.gray06, 0.8)};
-    border-radius: 4px;
-    padding: 12px 8px;
-    width: 100%;
-    text-align: center;
-    line-height: 30px;
-    white-space: pre-wrap;
+  ${({ theme }) => theme.typography.header03}
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => hexToRgba(theme.colors.gray06, 0.8)};
+  border-radius: 4px;
+  padding: 12px 8px;
+  width: 100%;
+  text-align: center;
+  line-height: 30px;
+  white-space: pre-wrap;
 `;
 
 export const Description = styled.p`
-    ${({ theme }) => theme.typography.bodyRegular}
-    color: ${({ theme }) => theme.colors.white};
-    text-align: center;
-    line-height: 30px;
-    white-space: pre-wrap;
+  ${({ theme }) => theme.typography.bodyRegular}
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
+  line-height: 30px;
+  white-space: pre-wrap;
 `;
 
 export const TitleImage = styled.img`
-    width: 210px;
+  width: 210px;
 `;
 
 export const ScreenshotBoxContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 128px;
-    align-items: center;
-    width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 128px;
+  align-items: center;
+  width: 80%;
 `;
 
 export const ScreenshotBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    gap: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
 `;
 
 export const Bubble = styled.div<{
   $direction?: 'left' | 'right';
   $color?: 'light' | 'dark';
 }>`
-    ${({ theme }) => theme.typography.bodyLarge}
-    color: ${({ $color, theme }) => ($color === 'light' ? theme.colors.gray06 : theme.colors.white)};
-    background-color: ${({ $color, theme }) => hexToRgba($color === 'light' ? theme.colors.gray01 : theme.colors.gray06, 0.8)};
-    width: 100%;
-    padding: 12px 8px;
-    line-height: 36px;
-    white-space: pre-wrap;
-    text-align: center;
-    border-radius: 24px;
+  ${({ theme }) => theme.typography.bodyLarge}
+  color: ${({ $color, theme }) => ($color === 'light' ? theme.colors.gray06 : theme.colors.white)};
+  background-color: ${({ $color, theme }) =>
+    hexToRgba($color === 'light' ? theme.colors.gray01 : theme.colors.gray06, 0.8)};
+  width: 100%;
+  padding: 12px 8px;
+  line-height: 36px;
+  white-space: pre-wrap;
+  text-align: center;
+  border-radius: 24px;
 `;
 
 export const ScreenshotDescription = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    flex: 1;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  flex: 1;
+  justify-content: center;
 `;
 
 export const Screenshot = styled.img`
-    width: 100%;
+  width: 100%;
 `;
