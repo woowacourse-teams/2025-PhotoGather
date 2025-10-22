e53fa6ad2da301a6803662166353e21589e114e6
kjyyjk <rlawnsdud920@gmail.com>
2025-10-22 13:08:04 +0900

    refactor: 주석 제거 및 체이닝 메서드를 가변 인자로 변경

--- backend/forgather/src/main/java/com/forgather/global/config/WebConfig.java
+++ backend/forgather/src/main/java/com/forgather/global/config/WebConfig.java
@@ -48,8 +48,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor)
-            .excludePathPatterns("/swagger-ui/**")
-            .excludePathPatterns("/v3/api-docs/**");
+            .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**");
     }
 
     @Override
