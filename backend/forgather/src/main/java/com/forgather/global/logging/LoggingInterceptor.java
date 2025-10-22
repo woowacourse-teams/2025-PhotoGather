c352862b12be9e9209ef97cafcefc06ca0c48d25
kjyyjk <rlawnsdud920@gmail.com>
2025-10-22 11:04:15 +0900

    refactor: 응답 로그에도 요청에 관한 정보 추가

--- backend/forgather/src/main/java/com/forgather/global/logging/LoggingInterceptor.java
+++ backend/forgather/src/main/java/com/forgather/global/logging/LoggingInterceptor.java
@@ -82,6 +82,9 @@ public class LoggingInterceptor implements HandlerInterceptor {
 
         log.atTrace()
             .addKeyValue("event", "RESPONSE")
+            .addKeyValue("httpMethod", request.getMethod())
+            .addKeyValue("requestUri", request.getRequestURI())
+            .addKeyValue("queryString", request.getQueryString())
             .addKeyValue("duration", durationMillis + "ms")
             .log();
 
