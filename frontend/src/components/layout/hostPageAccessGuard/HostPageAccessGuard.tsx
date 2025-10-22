0c68297aa74b946c0c34a9f65e01b8f005a9f775
hanheel <hanhee630630@gmail.com>
2025-10-22 18:00:18 +0900

    feat: HostPageAccessGuard 페이지  생성
    추후 API 연동 필요

--- frontend/src/components/layout/hostPageAccessGuard/HostPageAccessGuard.tsx
+++ frontend/src/components/layout/hostPageAccessGuard/HostPageAccessGuard.tsx
@@ -1,14 +1,8 @@
-// import { useContext } from 'react';
-// import { UserContext } from '../../../contexts/UserContext';
+import { Outlet } from 'react-router-dom';
 
-// const HostPageAccessGuard = () => {
-//   // 1. 로그인이 되어있지 않으면 PrivateRoute 에서 걸러줌
-//   // 2. 로그인이 되어있지만 호스트 페이지에 접근하려고 하면 걸러줌
-//   const { userInfo } = useContext(UserContext);
+const HostPageAccessGuard = () => {
+  // TODO : 소유자 정보와 공간 정보를 비교하는 API 연동 필요
+  return <Outlet />;
+};
 
-//   if(!userInfo){
-//     return <Navigate to={ROUTES.AUTH.LOGIN} />;
-//   }
-// };
-
-// export default HostPageAccessGuard;
+export default HostPageAccessGuard;
