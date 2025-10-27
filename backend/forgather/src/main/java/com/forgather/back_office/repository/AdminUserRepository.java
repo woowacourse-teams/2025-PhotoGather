cfd9544520d8832be6c8e22ce7e2c443d98f1be0
ykmxxi <ykmxxi97@gmail.com>
2025-10-27 20:10:56 +0900

    test: 어드민 로그인 인수 테스트

--- backend/forgather/src/main/java/com/forgather/back_office/repository/AdminUserRepository.java
+++ backend/forgather/src/main/java/com/forgather/back_office/repository/AdminUserRepository.java
@@ -5,5 +5,8 @@ import java.util.Optional;
 import com.forgather.back_office.model.AdminUser;
 
 public interface AdminUserRepository {
+
+    AdminUser save(AdminUser adminUser);
+
     Optional<AdminUser> findByUsername(String username);
 }
