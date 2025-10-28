16b2e5a13f6cf1a33487cfeb6c0ab9c57d44368f
Minje Sung <minje813@dgu.ac.kr>
2025-10-28 22:36:15 +0900

    fix: 기본값 설정 오류 해결

--- frontend/src/styles/@common/DividerLine.styles.ts
+++ frontend/src/styles/@common/DividerLine.styles.ts
@@ -3,5 +3,5 @@ import styled from '@emotion/styled';
 export const DividerLine = styled.div<{ width: string; color?: string }>`
   width: ${({ width }) => width};
   height: 1px;
-  background-color: ${({ theme, color = theme.colors.gray02 }) => color};
+  background-color: ${({ theme, color }) => color ?? theme.colors.gray02};
 `;
