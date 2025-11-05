9662d6a3e1889c56e170e5aec60d22226a353220
ykmxxi <ykmxxi97@gmail.com>
2025-11-05 23:59:34 +0900

    refactor: null 오류 변경

--- backend/forgather/src/main/java/com/forgather/back_office/repository/AdminUserRepository.java
+++ backend/forgather/src/main/java/com/forgather/back_office/repository/AdminUserRepository.java
@@ -2,8 +2,10 @@ package com.forgather.back_office.repository;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+
 import com.forgather.back_office.model.AdminUser;
-import com.forgather.global.exception.BaseException;
+import com.forgather.global.exception.BaseNullPointerException;
 import com.forgather.global.exception.NotFoundException;
 
 public interface AdminUserRepository {
@@ -16,7 +18,7 @@ public interface AdminUserRepository {
 
     default AdminUser getByIdOrThrow(Long id) {
         if (id == null) {
-            throw new BaseException("어드민유저의 id는 null일 수 없습니다. id: " + id);
+            throw new BaseNullPointerException("어드민유저의 id는 null일 수 없습니다. id: " + id, HttpStatus.BAD_REQUEST);
         }
         return findById(id)
             .orElseThrow(() -> new NotFoundException("존재하지 않는 어드민유저입니다. id: " + id));
