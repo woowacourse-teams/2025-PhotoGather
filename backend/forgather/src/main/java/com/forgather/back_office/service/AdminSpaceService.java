35e87133780afce6ad60c69b267c9e25013d10a8
ykmxxi <ykmxxi97@gmail.com>
2025-11-13 16:34:08 +0900

    feat: 어드민 스페이스 상세 조회

--- backend/forgather/src/main/java/com/forgather/back_office/service/AdminSpaceService.java
+++ backend/forgather/src/main/java/com/forgather/back_office/service/AdminSpaceService.java
@@ -3,9 +3,13 @@ package com.forgather.back_office.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.forgather.back_office.dto.AdminSpaceResponse;
+import com.forgather.back_office.dto.SpaceDetailResponse;
 import com.forgather.back_office.model.AdminUser;
+import com.forgather.domain.guestbook.repository.GuestBookCardRepository;
+import com.forgather.domain.product.repository.ProductRepository;
 import com.forgather.domain.space.model.Space;
 import com.forgather.domain.space.repository.SpaceRepository;
 
@@ -16,10 +20,23 @@ import lombok.RequiredArgsConstructor;
 public class AdminSpaceService {
 
     private final SpaceRepository spaceRepository;
+    private final ProductRepository productRepository;
+    private final GuestBookCardRepository guestBookCardRepository;
 
+    @Transactional(readOnly = true)
     public AdminSpaceResponse getAllSpaces(Pageable pageable, AdminUser adminUser) {
         // TODO: 관리자 권한에 따른 호출 여부 로직 추가 가능성이 있음
         Page<Space> spaces = spaceRepository.findAll(pageable);
         return AdminSpaceResponse.from(spaces);
+    }
+
+    @Transactional(readOnly = true)
+    public SpaceDetailResponse getSpaceDetail(String spaceCode, AdminUser adminUser) {
+        // 필요한 정보: 해당 스페이스의 회원, 해당 스페이스 작품 소개 존재 여부, 해당 해당 스페이스의 방명록 개수,
+        Space space = spaceRepository.getByCodeOrThrow(spaceCode);
+        boolean hasProduct = productRepository.existsBySpace(space);
+        Long guestBookCardCount = guestBookCardRepository.countBySpace(space);
+
+        return SpaceDetailResponse.of(space, hasProduct, guestBookCardCount);
     }
 }
--- backend/forgather/src/main/java/com/forgather/domain/product/repository/ProductRepository.java
+++ backend/forgather/src/main/java/com/forgather/domain/product/repository/ProductRepository.java
@@ -16,6 +16,8 @@ public interface ProductRepository {
 
     Optional<Product> findBySpace(Space space);
 
+    boolean existsBySpace(Space space);
+
     default Product getBySpaceOrThrow(Space space) {
         if (space == null) {
             throw new BaseNullPointerException("스페이스는 null일 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
