85ccf54f97535a15b9b2c0969ebd3906b202ff6d
H0ngJu <hongju4385@gmail.com>
2025-11-19 19:17:23 +0900

    feat: 일반 유튜브 공유 링크를 임베드링크로 변경하는 유틸 함수 분리

---
+++ frontend/src/utils/buildYoutubeEmbedLink.ts
@@ -0,0 +1,10 @@
+// https://www.youtube.com/embed/KsGBitYXn3M?si=Cd6nxQpAKrVpCe4E
+// https://youtu.be/embed/KsGBitYXn3M?si=vEtbIFtzQ9srNwa3
+
+export const buildYoutubeEmbedLink = (youtubeUrl: string) => {
+  if (youtubeUrl.includes('embed')) {
+    return youtubeUrl;
+  }
+  const parsedUrl = youtubeUrl.replace('youtu.be/', 'www.youtube.com/embed/');
+  return parsedUrl;
+};
