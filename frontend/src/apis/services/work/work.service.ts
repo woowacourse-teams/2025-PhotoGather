b6edc866b477a63aa238b4d5520bae2aaad51fd6
H0ngJu <hongju4385@gmail.com>
2025-11-22 23:43:24 +0900

    feat: getWorks api 추가

--- frontend/src/apis/services/work/work.service.ts
+++ frontend/src/apis/services/work/work.service.ts
@@ -2,15 +2,22 @@ import type {
   CreateWorkRequest,
   UpdateWorkRequest,
   WorkDetail,
+  WorkListResponse,
 } from '../../../types/domain/work.type';
 import { http } from '../../http';
 
-const API_VERSION_HEADERS = { 'X-API-Version': '2' };
+const API_VERSION_V2_HEADERS = { 'X-API-Version': '2' };
+const API_VERSION_V3_HEADERS = { 'X-API-Version': '3' };
 
 export const workService = {
+  getWorks: (spaceCode: string) =>
+    http.get<WorkListResponse>(`/spaces/${spaceCode}/products`, {
+      headers: API_VERSION_V3_HEADERS,
+    }),
+
   getWork: (spaceCode: string) =>
     http.get<WorkDetail>(`/spaces/${spaceCode}/products`, {
-      headers: API_VERSION_HEADERS,
+      headers: API_VERSION_V2_HEADERS,
     }),
 
   deleteWork: (spaceCode: string) =>
@@ -19,14 +26,14 @@ export const workService = {
   createWork: (spaceCode: string, data: CreateWorkRequest) => {
     return http.post(`/spaces/${spaceCode}/products`, {
       body: data,
-      headers: API_VERSION_HEADERS,
+      headers: API_VERSION_V2_HEADERS,
     });
   },
 
   updateWork: (spaceCode: string, data: UpdateWorkRequest) => {
     return http.patch(`/spaces/${spaceCode}/products`, {
       body: data,
-      headers: API_VERSION_HEADERS,
+      headers: API_VERSION_V2_HEADERS,
     });
   },
 };
