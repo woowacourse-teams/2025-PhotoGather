cd543f733c3adf88455991aca86a6bfe123bfcbd
H0ngJu <hongju4385@gmail.com>
2025-11-23 15:21:44 +0900

    refactor: 비디오 아이디 11글자 제한 추가

--- frontend/src/utils/checkIsYoutube.ts
+++ frontend/src/utils/checkIsYoutube.ts
@@ -1,6 +1,6 @@
 export const checkIsYoutube = (youtubeUrl: string) => {
   const youtubeRegex =
-    /^(https?:\/\/)(www\.)?(m\.)?(youtube\.com\/(watch\?v=.+|shorts\/.+|embed\/.+)|youtu\.be\/.+)/;
+    /^(https?:\/\/)(www\.)?(m\.)?(youtube\.com\/(watch\?v=[A-Za-z0-9_-]+|shorts\/[A-Za-z0-9_-]+|embed\/[A-Za-z0-9_-]+)|youtu\.be\/[A-Za-z0-9_-]+)/;
 
   return youtubeRegex.test(youtubeUrl);
 };
