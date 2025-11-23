ccac52fb4499370093ac67dff9b82448a284ba78
H0ngJu <hongju4385@gmail.com>
2025-11-23 14:59:51 +0900

    refactor: API_VERSION 리팩터링

--- frontend/src/apis/services/work/work.service.ts
+++ frontend/src/apis/services/work/work.service.ts
@@ -6,37 +6,39 @@ import type {
 } from '../../../types/domain/work.type';
 import { http } from '../../http';
 
-const API_VERSION_V2_HEADERS = { 'X-API-Version': '2' };
-const API_VERSION_V3_HEADERS = { 'X-API-Version': '3' };
-const API_VERSION_V1_HEADERS = { 'X-API-Version': '1' };
+export const API_VERSION = {
+  V1: { 'X-API-Version': '1' },
+  V2: { 'X-API-Version': '2' },
+  V3: { 'X-API-Version': '3' },
+} as const;
 
 export const workService = {
   getWorks: (spaceCode: string) =>
     http.get<WorkListResponse>(`/spaces/${spaceCode}/products`, {
-      headers: API_VERSION_V3_HEADERS,
+      headers: API_VERSION.V3,
     }),
 
   getWork: (spaceCode: string, workId: string) =>
     http.get<WorkDetail>(`/spaces/${spaceCode}/products/${workId}`, {
-      headers: API_VERSION_V1_HEADERS,
+      headers: API_VERSION.V1,
     }),
 
   deleteWork: (spaceCode: string, workId: string) =>
     http.delete<WorkDetail>(`/spaces/${spaceCode}/products/${workId}`, {
-      headers: API_VERSION_V1_HEADERS,
+      headers: API_VERSION.V1,
     }),
 
   createWork: (spaceCode: string, data: CreateWorkRequest) => {
     return http.post<WorkDetail>(`/spaces/${spaceCode}/products`, {
       body: data,
-      headers: API_VERSION_V3_HEADERS,
+      headers: API_VERSION.V3,
     });
   },
 
   updateWork: (spaceCode: string, workId: string, data: UpdateWorkRequest) => {
     return http.patch<WorkDetail>(`/spaces/${spaceCode}/products/${workId}`, {
       body: data,
-      headers: API_VERSION_V1_HEADERS,
+      headers: API_VERSION.V1,
     });
   },
 };
