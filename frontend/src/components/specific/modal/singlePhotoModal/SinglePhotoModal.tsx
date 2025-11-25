d67da9626baed18d6c3df780fcd145937837d150
H0ngJu <hongju4385@gmail.com>
2025-11-25 13:53:07 +0900

    fix: SinglePhotoModal에 Image onError 추가

--- frontend/src/components/specific/modal/singlePhotoModal/SinglePhotoModal.tsx
+++ frontend/src/components/specific/modal/singlePhotoModal/SinglePhotoModal.tsx
@@ -1,3 +1,6 @@
+import defaultImage from '../../../../@assets/images/default-forgather-image.png';
+import { createImageErrorHandler } from '../../../../utils/createImageErrorHandler';
+
 import Modal from '../../../@common/modal/Modal';
 import * as S from './SinglePhotoModal.style';
 
@@ -12,12 +15,17 @@ const SinglePhotoModal = ({
   isOpen,
   imgSrc,
 }: SinglePhotoModalProps) => {
+  console.log('imgSrc', imgSrc);
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <Modal.Backdrop />
       <S.Wrapper>
         <S.ImageWrapper>
-          <S.Image src={imgSrc} alt="사진" />
+          <S.Image
+            src={imgSrc}
+            alt="사진"
+            onError={createImageErrorHandler(defaultImage)}
+          />
         </S.ImageWrapper>
       </S.Wrapper>
     </Modal>
