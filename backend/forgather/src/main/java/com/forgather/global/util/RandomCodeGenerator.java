493af104e6a9fb3efd71d434604fdc7f4b2a877f
ykmxxi <ykmxxi97@gmail.com>
2025-12-02 19:35:19 +0900

    refactor: 랜덤 코드 생성 로직 개선
    - SecureRandom 보안성 강화
    - 기존 경우의 수(16^length) -> 바뀐 경우의 수(36^length)

--- backend/forgather/src/main/java/com/forgather/global/util/RandomCodeGenerator.java
+++ backend/forgather/src/main/java/com/forgather/global/util/RandomCodeGenerator.java
@@ -1,22 +1,25 @@
 package com.forgather.global.util;
 
-import java.util.UUID;
+import java.security.SecureRandom;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomCodeGenerator {
 
+    private static final String NUMBER_AND_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
     public String generate(int length) {
         if (length <= 0) {
             return "";
         }
-
         StringBuilder randomString = new StringBuilder();
-        while (randomString.length() < length) {
-            randomString.append(UUID.randomUUID().toString().replace("-", ""));
+        for (int i = 0; i < length; i++) {
+            randomString.append(NUMBER_AND_ALPHABET.charAt(
+                RANDOM.nextInt(NUMBER_AND_ALPHABET.length())
+            ));
         }
-
-        return randomString.substring(0, length);
+        return randomString.toString();
     }
 }
