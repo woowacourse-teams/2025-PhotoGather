fa4b2d93bfaa6e02b6acb0af4ba991cec58bd1ef
ykmxxi <ykmxxi97@gmail.com>
2025-12-02 22:35:23 +0900

    refactor: ThreadLocalRandom을 사용하도록 변경

--- backend/forgather/src/main/java/com/forgather/global/util/RandomCodeGenerator.java
+++ backend/forgather/src/main/java/com/forgather/global/util/RandomCodeGenerator.java
@@ -1,6 +1,6 @@
 package com.forgather.global.util;
 
-import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.stereotype.Component;
 
@@ -8,16 +8,16 @@ import org.springframework.stereotype.Component;
 public class RandomCodeGenerator {
 
     private static final String NUMBER_AND_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
-    private static final SecureRandom RANDOM = new SecureRandom();
 
     public String generate(int length) {
         if (length <= 0) {
             return "";
         }
         StringBuilder randomString = new StringBuilder();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
         for (int i = 0; i < length; i++) {
             randomString.append(NUMBER_AND_ALPHABET.charAt(
-                RANDOM.nextInt(NUMBER_AND_ALPHABET.length())
+                random.nextInt(NUMBER_AND_ALPHABET.length())
             ));
         }
         return randomString.toString();
