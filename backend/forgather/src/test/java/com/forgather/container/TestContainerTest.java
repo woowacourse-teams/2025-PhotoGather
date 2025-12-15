129864ac4f574763bd1debd6495bd313684c811a
kjyyjk <rlawnsdud920@gmail.com>
2025-12-15 19:55:55 +0900

    test: 테스트 내 public 접근 제어자 삭제

--- backend/forgather/src/test/java/com/forgather/container/TestContainerTest.java
+++ backend/forgather/src/test/java/com/forgather/container/TestContainerTest.java
@@ -5,11 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TestContainerTest extends TestOnContainer {
+class TestContainerTest extends TestOnContainer {
 
     @DisplayName("MySQL 테스트 컨테이너가 정상적으로 가동된다")
     @Test
-    public void testContainer() {
+    void testContainer() {
         // then
         assertThat(mysql.isRunning()).isTrue();
     }
