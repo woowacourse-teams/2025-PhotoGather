2f3be2c005c62422b5a05370b6d08d26958ce6ac
kjyyjk <rlawnsdud920@gmail.com>
2025-12-15 17:16:58 +0900

    test: 스페이스 저장 시간이 동일해 의도하지 않은 정렬 문제 해결
    - 현재 createdAt은 초 단위까지만 저장하고, ms 단위는 저장하지 않는다.
    따라서 ms 단위의 저장 시점 차이는 동일한 시간으로 간주된다.
    의도한 테스트 환경을 구성하기 위해 1초 sleep을 통한 저장 시점 차이를 둔다.

--- backend/forgather/src/test/java/com/forgather/acceptance/SpaceAcceptanceTest.java
+++ backend/forgather/src/test/java/com/forgather/acceptance/SpaceAcceptanceTest.java
@@ -464,10 +464,11 @@ class SpaceAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("나의 스페이스 목록을 조회한다.")
     @Test
-    void getSpaces() {
+    void getSpaces() throws InterruptedException {
         // given
         Space space1 = spaceRepository.save(SpaceFixture.createSpace());
         spacePhotoRepository.save(SpacePhotoFixture.createSpacePhotoWithSpace(space1));
+        Thread.sleep(1000);
         Space space2 = spaceRepository.save(SpaceFixture.createPrivateSpace());
         spacePhotoRepository.save(SpacePhotoFixture.createSpacePhotoWithSpace(space2));
         spaceHostMapRepository.save(new SpaceHostMap(space1, host));
@@ -492,7 +493,7 @@ class SpaceAcceptanceTest extends AcceptanceTest {
             () -> assertThat(result.spaces().getFirst().guestBookCardCount()).isZero(),
 
             () -> assertThat(result.spaces().getLast().spaceCode()).isEqualTo(space1.getCode()),
-            () -> assertThat(result.spaces().getLast().guestBookCardCount()).isEqualTo(1)
+            () -> assertThat(result.spaces().getLast().guestBookCardCount()).isOne()
         );
     }
 }
