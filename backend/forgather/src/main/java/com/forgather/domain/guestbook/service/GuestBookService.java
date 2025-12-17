68e618a63919bcf288dfbe84586b153279723008
kjyyjk <rlawnsdud920@gmail.com>
2025-12-17 16:21:32 +0900

    refactor: 방명록 관련 엔티티 삭제 시 논리 삭제한다

--- backend/forgather/src/main/java/com/forgather/domain/guestbook/service/GuestBookService.java
+++ backend/forgather/src/main/java/com/forgather/domain/guestbook/service/GuestBookService.java
@@ -7,7 +7,6 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -31,7 +30,6 @@ import com.forgather.domain.guestbook.repository.dto.GuestBookCardListDto;
 import com.forgather.domain.space.model.Space;
 import com.forgather.domain.space.repository.SpaceRepository;
 import com.forgather.domain.upload.domain.ContentsStorage;
-import com.forgather.domain.upload.event.DeletePhotoEvent;
 import com.forgather.global.auth.model.Host;
 import com.forgather.global.auth.repository.SpaceHostMapRepository;
 import com.forgather.global.exception.BaseNullPointerException;
@@ -44,7 +42,6 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class GuestBookService {
 
-    private final ApplicationEventPublisher eventPublisher;
     private final SpaceRepository spaceRepository;
     private final SpaceHostMapRepository spaceHostMapRepository;
     private final GuestRepository guestRepository;
@@ -138,14 +135,12 @@ public class GuestBookService {
         validateSpaceHost(host, space);
         GuestBookCard guestBookCard = getGuestBookCard(guestBookCardId, space);
         deleteGuestBookCardPhotos(guestBookCard);
-        guestBookCardRepository.delete(guestBookCard);
+        guestBookCard.delete();
     }
 
     private void deleteGuestBookCardPhotos(GuestBookCard guestBookCard) {
         List<GuestBookCardPhoto> photos = guestBookCardPhotoRepository.findAllByGuestBookCardAndDeletedAtIsNull(guestBookCard);
-        if (!photos.isEmpty()) {
-            deleteGuestBookCardPhotos(photos);
-        }
+        deleteGuestBookCardPhotos(photos);
     }
 
     @Transactional
@@ -195,7 +190,8 @@ public class GuestBookService {
     }
 
     private void deleteGuestBookCardPhotos(List<GuestBookCardPhoto> photos) {
-        guestBookCardPhotoRepository.deleteAll(photos);
-        eventPublisher.publishEvent(new DeletePhotoEvent(this, photos)); // 클라우드 삭제 이벤트 발행
+        for (GuestBookCardPhoto photo : photos) {
+            photo.delete();
+        }
     }
 }
