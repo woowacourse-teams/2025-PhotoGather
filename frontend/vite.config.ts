5a4fa401039da173dfd8c210eb0cbe1e5e985067
hanheel <hanhee630630@gmail.com>
2026-01-20 00:54:38 +0900

    refactor: vite-imagetools defaultDirectives 설정 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,12 +6,34 @@ import svgr from 'vite-plugin-svgr';
 
 const isProduction = process.env.VITE_ENVIRONMENT === 'production';
 
+const DEFAULT_IMAGE_CONFIG = {
+  format: 'webp',
+  w: '600',
+};
+
+const IMAGE_CONFIG_OPTION = {
+  'space-create.png': { w: '300' },
+} as const;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    imagetools(),
+    imagetools({
+      defaultDirectives: (url: URL) => {
+        const isImage = /\.(png|jpg|jpeg)$/.test(url.pathname);
+        if (!isImage) return new URLSearchParams();
+
+        const fileName = url.pathname.split('/').pop() || '';
+        const OVERRIDE_CONFIG = IMAGE_CONFIG_OPTION[fileName as keyof typeof IMAGE_CONFIG_OPTION];
+
+        return new URLSearchParams({
+          ...DEFAULT_IMAGE_CONFIG,
+          ...OVERRIDE_CONFIG,
+        });
+      },
+    }),
     isProduction
       ? sentryVitePlugin({
           org: process.env.VITE_SENTRY_ORG,
